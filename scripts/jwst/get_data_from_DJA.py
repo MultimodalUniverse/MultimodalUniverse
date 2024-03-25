e2121514ccce29f5e7919bee82ae4e71707e1b15
Kartheik Iyer <protopotato@gmail.com>
2024-03-25 17:47:25 -0400

    Update get_data_from_DJA.py to get RA/DEC
    add the eazy fits for RA/DEC

--- scripts/jwst/get_data_from_DJA.py
+++ scripts/jwst/get_data_from_DJA.py
@@ -2,12 +2,20 @@ import numpy as np
 import json
 import wget
 from bs4 import BeautifulSoup
+import os
+import tarfile
+from astropy.table import Table
 
 base_url = 'https://s3.amazonaws.com/grizli-v2/JwstMosaics/v6/index.html'
-output_directory = 'data/'
+output_directory = 'cosmos_web/'
+try:
+    os.chdir(output_directory)
+    os.chdir('..')
+except:
+    print('output directory not found, made the dir.')
+    os.mkdir(output_directory)
 
 file = wget.download(base_url)
-
 
 f = open(file, "r")
 file_content = f.read()
@@ -29,3 +37,20 @@ for i in range(len(cosmos_files)):
     print('url: ', cosmos_files[i])
 
     file = wget.download(cosmos_files[i], out=output_directory)
+    
+for temp in soup.find_all('a'):
+    if ('cosweb' in temp['href']) & ('photoz' in temp['href']):
+        print(temp['href'])
+        photoz_url = temp['href']
+
+file = wget.download(photoz_url, out=output_directory)
+
+tar = tarfile.open(file)
+tar.extractall(path=output_directory)
+tar.close()
+
+fnames = os.listdir(output_directory)
+for fname in fnames:
+    if 'eazypy.zout' in fname:
+        phot_table = Table.read(output_directory + fname)
+phot_table
