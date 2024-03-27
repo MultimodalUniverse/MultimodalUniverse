4901d3133efa04a458a5b831a065fae13684f411
Liam Parker <lparker@cmbaslin030.flatironinstitute.org>
2024-03-27 16:45:37 -0400

    update for splitting

--- baselines/photo_z/photo_z_wrapper.py
+++ baselines/photo_z/photo_z_wrapper.py
@@ -3,12 +3,13 @@ sys.path.append('../')
 import torch
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import Dataset, DataLoader
-from utils import split_dataset, compute_dataset_statistics, normalize_sample
+from utils import split_dataset, compute_dataset_statistics, normalize_sample, get_nested
 from typing import Any
 
 class PhotoZWrapper(LightningDataModule):
     def __init__(self, 
-                 dataset: Dataset, 
+                 train_dataset: Dataset, 
+                 test_dataset: Dataset,
                  batch_size: int = 128, 
                  num_workers: int = 8, 
                  test_size: float = 0.2, 
@@ -33,24 +34,25 @@ class PhotoZWrapper(LightningDataModule):
         - dynamic_range (bool): Flag indicating whether dynamic range compression should be applied.
         """
 
-        self.dataset = dataset
+        self.train_dataset = train_dataset
+        self.test_dataset = test_dataset
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.test_size = test_size
-        self.split_method = split_method
         self.loading = loading
         self.dynamic_range = dynamic_range
         self.feature_flag = feature_flag
         self.label_flag = label_flag
 
     def prepare_data(self):
-        # Split the dataset
-        self.train_dataset, self.test_dataset = split_dataset(self.dataset, split=self.split_method)
-
         # Compute the dataset statistics
         self.img_mean, self.img_std = compute_dataset_statistics(self.train_dataset, flag=self.feature_flag, loading=self.loading)
         self.z_mean, self.z_std = compute_dataset_statistics(self.train_dataset, flag=self.label_flag, loading=self.loading)
 
+        # For correct broadcasting
+        self.img_mean, self.img_std = self.img_mean[:,None,None], self.img_std[:,None,None]
+
+        # Split the dataset
         train_test_split = self.train_dataset.train_test_split(test_size=self.test_size)
         self.train_dataset = train_test_split['train']
         self.val_dataset = train_test_split['test']
@@ -60,9 +62,9 @@ class PhotoZWrapper(LightningDataModule):
 
     def collate_fn(self, batch):
         batch = torch.utils.data.default_collate(batch)
-        batch['image'] = normalize_sample(batch['image'], self.img_mean, self.img_std, dynamic_range=self.dynamic_range).permute(0, 3, 1, 2) # dynamic range compression and z-score normalization
-        batch['redshift'] = normalize_sample(batch['redshift'], self.z_mean, self.z_std, dynamic_range=False)
-        return batch['image'], batch['redshift']
+        x = normalize_sample(get_nested(batch, self.feature_flag), self.img_mean, self.img_std, dynamic_range=self.dynamic_range) # dynamic range compression and z-score normalization
+        y = normalize_sample(get_nested(batch, self.label_flag), self.z_mean, self.z_std, dynamic_range=False)
+        return x, y
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=self.collate_fn)
