238989a6ea04974a3974b9677591ab5f9d3353db
Tom Hehir CSD3 <148493038+tom-hehir@users.noreply.github.com>
2024-03-28 10:32:17 +0000

    got new hdf5 to work

--- scripts/yse/build_parent_sample.py
+++ scripts/yse/build_parent_sample.py
@@ -1,49 +1,111 @@
+# %%
 import h5py
 import numpy as np
 import sncosmo
 import glob
 
-def get_str_dtype(value):
-    str_max_len = int(np.char.str_len(value).max())
+# %%
+def get_str_dtype(arr):
+    str_max_len = int(np.char.str_len(arr).max())
     return h5py.string_dtype(encoding='utf-8', length=str_max_len)
 
+def convert_dtype(arr):
+    if np.issubdtype(arr.dtype, np.floating):
+        dtype = np.float32
+    elif np.issubdtype(arr.dtype, np.str_):
+        dtype = get_str_dtype(arr)
+    else:
+        dtype = arr.dtype
+    return arr.astype(dtype)
+
+# %%
 file_paths = glob.glob(r'../../data/yse_dr1_zenodo/*.dat')
 num_examples = len(file_paths)
 
-field = ['object_id', 'ra', 'dec', 'time', 'flux', 'flux_err', 'band', 'quality_mask', 'redshift', 'host_log_mass', 'spec_class']
-key = ['SNID', 'RA', 'DECL', 'MJD', 'FLUXCAL', 'FLUXCALERR', 'FLT', 'FLAG', 'REDSHIFT_FINAL', 'HOST_LOGMASS', 'SPEC_CLASS']
-partition = ['metadata'] * 3 + ['data'] * 5 + ['metadata'] * 3
-length = []
-value = dict(zip(field, ([] for _ in field)))
+# %%
+"""
+ignored_keys = {'PARSNIP_S1', 'PARSNIP_S2', 'PARSNIP_S3', 'SET_ZTF_FP', 'processing', 'SEARCH_PEAKFLT', 'END'}
+
+example_metadata, example_data = sncosmo.read_snana_ascii(file_paths[0], default_tablename='OBS')
+example_data = example_data['OBS']
+key_metadata = [key for key in example_metadata.keys() if key not in ignored_keys]
+#key_metadata = list(example_metadata.keys())
+key_data = [key for key in example_data.keys() if key not in ignored_keys]
+#key_data = example_data.keys()
+key_all = key_metadata + key_data
+name_conversion = dict(zip(key_all, key_all))
+name_conversion.update({
+    'SNID': 'object_id',
+    'RA': 'ra',
+    'DECL': 'dec',
+    'MJD': 'time',
+    'FLUXCAL': 'flux',
+    'FLUXCALERR': 'flux_err',
+    'FLT': 'band',
+    'FLAG': 'quality_mask',
+})
+field_data = {name_conversion[key] for key in key_data}
+field_metadata = {name_conversion[key] for key in key_metadata}
+"""
+
+field_data = ['time', 'flux', 'flux_err', 'band', 'quality_mask',]
+key_data = ['MJD', 'FLUXCAL', 'FLUXCALERR', 'FLT', 'FLAG',]
+
+field_metadata = ['object_id', 'ra', 'dec',  'redshift', 'host_log_mass', 'spec_class']
+key_metadata = ['SNID', 'RA', 'DECL', 'REDSHIFT_FINAL', 'HOST_LOGMASS', 'SPEC_CLASS']
+
+# %%
+data = dict(zip(field_data, ([] for _ in field_data)))
+metadata = dict(zip(field_metadata, ([] for _ in field_metadata)))
 
 for file_path in file_paths:
-    metadata, data = sncosmo.read_snana_ascii(file_path, default_tablename='OBS')
-    data = data['OBS']
-    length.append(len(data['MJD']))
-    for f, k, p in zip(field, key, partition):
-        p = metadata if p == 'metadata' else data
-        value[f].append(p[k])
+    metadata_, data_ = sncosmo.read_snana_ascii(file_path, default_tablename='OBS')
+    data_ = data_['OBS']
+    for field, key in zip(field_data, key_data):
+        data[field].append(data_[key].data)
+        # the data are astropy columns wrapping numpy arrays which are accessed via .data
+    for field, key in zip(field_metadata, key_metadata):
+        metadata[field].append(metadata_[key])
 
-length = np.array(length)
-max_len = np.max(length)
-pad = max_len - length
+# %%
+all_bands = np.unique(np.concatenate(data['band']))
+
+# %%
+max_length = 0
+for i in range(num_examples):
+    band, count = np.unique(data['band'][i], return_counts=True)
+    max_length = max(max_length, count.max())
+
+# %%
+field_data.remove('band')
+banded_data = dict(zip(field_data, ([] for _ in field_data)))
 
 for i in range(num_examples):
-    for f, p in zip(field, partition):
-        if p == 'data':
-            value[f][i] = np.pad(value[f][i], (0, pad[i]), 'constant', constant_values=(0, 0))
+    mask = np.expand_dims(all_bands, 1) == data['band'][i]
+    for field in field_data:
+        d = []
+        for j in range(len(all_bands)):
+            d_ = data[field][i][mask[j]]
+            d_ = np.pad(d_, (0, max_length - len(d_)), mode='constant', constant_values=np.nan)
+            d.append(d_)
+        banded_data[field].append(d)
 
-#dtype = ['S'+str(np.char.str_len(value['object_id']).max()), np.float32, np.float32, np.float32, np.float32, np.float32, 'S'+str(np.char.str_len(value['band']).max()), np.float32, np.float32, np.float32, np.float32]
-dtype = [get_str_dtype(value['object_id']), np.float32, np.float32, np.float32, np.float32, np.float32, get_str_dtype(value['band']), np.float32, np.float32, np.float32, get_str_dtype(value['spec_class'])]
+for field in field_data:
+    banded_data[field] = np.array(banded_data[field])
+banded_data['band'] = np.array((all_bands for _ in range(num_examples)))
 
+# %%
+for field in field_metadata:
+    metadata[field] = np.array(metadata[field])
+
+# %%
 with h5py.File('../../data/yse.hdf5', 'w') as hdf5_file:
-    for f, d in zip(field, dtype):
-        if f == 'quality_mask':
-            v = np.zeros((num_examples, max_len), dtype=d)
-            # NOTE: could also encode 0x00 and 0 as strings
-        else:
-            v = np.array(value[f], dtype=d)
-        hdf5_file.create_dataset(f, data=v)
+    for field in field_metadata:
+        hdf5_file.create_dataset(field, data=convert_dtype(metadata[field]))
+    for field in field_data:
+        hdf5_file.create_dataset(field, data=convert_dtype(banded_data[field]))
 
-if False: pass # TODO: delete .snana.dat files
+# %%
+
+
 
