84125d8b12498c39e3be30105b6358bea6b20bff
Francois Lanusse <fr.eiffel@gmail.com>
2024-03-28 19:29:10 -0400

    Adding wide layer

--- scripts/hsc/test.sh
+++ scripts/hsc/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # First build the parent sample for the deep field
-if python build_parent_sample.py pdr3_dud_22.5.sql . --rerun pdr3_dud_rev --tiny; then
+if python build_parent_sample.py pdr3_dud_22.5.sql . --rerun pdr3_dud_rev --num_processes 1 --tiny; then
     echo "Build parent sample for deep field successful"
 else
     echo "Build parent sample for deep field failed"
@@ -17,17 +17,17 @@ else
 fi
 
 # Then build this parent sample for the wide field 
-# if python build_parent_sample.py pdr3_wide_22.5.sql . --rerun pdr3_wide --tiny; then
-#     echo "Build parent sample for wide field successful"
-# else
-#     echo "Build parent sample for wide field failed"
-#     exit 1
-# fi
+if python build_parent_sample.py pdr3_wide_22.5.sql . --rerun pdr3_wide --num_processes 1 --tiny; then
+    echo "Build parent sample for wide field successful"
+else
+    echo "Build parent sample for wide field failed"
+    exit 1
+fi
 
-# # Try to load the dataset with hugging face dataset
-# if python -c "from datasets import load_dataset; dset = load_dataset('./hsc.py', 'pdr3_wide_22.5', trust_remote_code=True, split='train'); print(next(iter(dset)))"; then
-#     echo "Load dataset for wide field successful"
-# else
-#     echo "Load dataset for wide field failed"
-#     exit 1
-# fi
+# Try to load the dataset with hugging face dataset
+if python -c "from datasets import load_dataset; dset = load_dataset('./hsc.py', 'pdr3_wide_22.5', trust_remote_code=True, split='train'); print(next(iter(dset)))"; then
+    echo "Load dataset for wide field successful"
+else
+    echo "Load dataset for wide field failed"
+    exit 1
+fi
