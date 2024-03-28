72d9ae7255f77782ef79ceb6df6fd2f125d27e17
Liam Parker <lparker@cmbaslin030.flatironinstitute.org>
2024-03-28 10:35:38 -0400

    newest updated

--- baselines/photo_z/photo_z_wrapper.py
+++ baselines/photo_z/photo_z_wrapper.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../')
 import torch
-from pytorch_lightning import LightningDataModule
+from lightning import LightningDataModule
 from torch.utils.data import Dataset, DataLoader
 from utils import split_dataset, compute_dataset_statistics, normalize_sample, get_nested
 from typing import Any
@@ -10,14 +10,18 @@ class PhotoZWrapper(LightningDataModule):
     def __init__(self, 
                  train_dataset: Dataset, 
                  test_dataset: Dataset,
+                 feature_flag: str = 'image',
+                 label_flag: str = 'z',
+                 feature_dynamic_range: bool = True,
+                 feature_z_score: bool = True,
+                 label_dynamic_range: bool = False,
+                 label_z_score: bool = True,
                  batch_size: int = 128, 
                  num_workers: int = 8, 
                  test_size: float = 0.2, 
-                 split_method: str = 'naive', 
                  loading: str = 'iterated', 
-                 feature_flag: str = 'image',
-                 label_flag: str = 'z',
-                 dynamic_range: bool = True):
+                 ):
+        super().__init__()
         """
         Initializes the data module with a dataset that is already loaded, setting up parameters
         for data processing and batch loading.
@@ -36,21 +40,28 @@ class PhotoZWrapper(LightningDataModule):
 
         self.train_dataset = train_dataset
         self.test_dataset = test_dataset
+
+        self.feature_flag = feature_flag
+        self.feature_dynamic_range = feature_dynamic_range
+        self.feature_z_score = feature_z_score
+
+        self.label_flag = label_flag
+        self.label_dynamic_range = label_dynamic_range
+        self.label_z_score = label_z_score
+
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.test_size = test_size
         self.loading = loading
-        self.dynamic_range = dynamic_range
-        self.feature_flag = feature_flag
-        self.label_flag = label_flag
+
+        self.prepare_data_per_node = False
 
     def prepare_data(self):
         # Compute the dataset statistics
-        self.img_mean, self.img_std = compute_dataset_statistics(self.train_dataset, flag=self.feature_flag, loading=self.loading)
-        self.z_mean, self.z_std = compute_dataset_statistics(self.train_dataset, flag=self.label_flag, loading=self.loading)
-
-        # For correct broadcasting
-        self.img_mean, self.img_std = self.img_mean[:,None,None], self.img_std[:,None,None]
+        if self.feature_z_score:
+            self.feature_mean, self.feature_std = compute_dataset_statistics(self.train_dataset, flag=self.feature_flag, loading=self.loading)
+        if self.label_z_score:
+            self.label_mean, self.label_std = compute_dataset_statistics(self.train_dataset, flag=self.label_flag, loading=self.loading)
 
         # Split the dataset
         train_test_split = self.train_dataset.train_test_split(test_size=self.test_size)
@@ -62,8 +73,8 @@ class PhotoZWrapper(LightningDataModule):
 
     def collate_fn(self, batch):
         batch = torch.utils.data.default_collate(batch)
-        x = normalize_sample(get_nested(batch, self.feature_flag), self.img_mean, self.img_std, dynamic_range=self.dynamic_range) # dynamic range compression and z-score normalization
-        y = normalize_sample(get_nested(batch, self.label_flag), self.z_mean, self.z_std, dynamic_range=False)
+        x = normalize_sample(get_nested(batch, self.feature_flag), self.feature_mean, self.feature_std, dynamic_range=self.feature_dynamic_range, z_score=self.feature_z_score) # dynamic range compression and z-score normalization
+        y = normalize_sample(get_nested(batch, self.label_flag), self.feature_mean, self.feature_std, dynamic_range=self.label_dynamic_range, z_score=self.label_z_score)
         return x, y
 
     def train_dataloader(self):
@@ -74,3 +85,5 @@ class PhotoZWrapper(LightningDataModule):
     
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=self.collate_fn)
+    
+    
