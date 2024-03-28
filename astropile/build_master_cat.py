d438f39d9fac83f16f1efe49a2a81f827e6c4e8e
Connor Stone <connorstone628@gmail.com>
2024-03-28 10:09:24 -0400

    Working out the logic

--- astropile/build_master_cat.py
+++ astropile/build_master_cat.py
@@ -8,9 +8,7 @@ import pandas as pd
 
 def extract_cat_params(cat: DatasetBuilder):
     cat = get_catalog(cat)
-    subcat = pd.DataFrame(
-        data=[cat[col] for col in ["ra", "dec", "healpix"]], columns=["ra", "dec", "healpix"]
-    )
+    subcat = pd.DataFrame(data=dict((col, cat[col].data) for col in ["ra", "dec", "healpix"]))
     return subcat
 
 
@@ -32,24 +30,44 @@ def build_catalogue(cats: list[DatasetBuilder], names: list[str], matching_radiu
         mask = sep2d < matching_radius * units.arcsec
 
         # Update the matching columns
-        master_cat[name][idx[mask]] = True
-        master_cat[name + "_idx"][idx[mask]] = idx[mask]
+        master_cat[name][mask] = True
+        master_cat[name + "_idx"][mask] = idx[mask]
 
         # Add new rows to the master catalogue
+        if len(master_cat) == 0:
+            mask = np.zeros(len(cat), dtype=bool)
+            print("First catalogue")
+            print(mask.shape)
+        else:
+            idx, sep2d, _ = cat_coords.match_to_catalog_sky(master_coords)
+            mask = sep2d < matching_radius * units.arcsec
+            print("adding new catalogue")
+            print(mask.shape)
+        idx = np.arange(len(cat), dtype=int)
         name_data = []
         name_idx_data = []
         for subname in names:
             if subname != name:
                 name_data.append(np.zeros(np.sum(~mask), dtype=bool))
-                name_idx_data.append(np.nan * np.oness(np.sum(~mask), dtype=int))
+                name_idx_data.append(-np.ones(np.sum(~mask), dtype=int))
             else:
                 name_data.append(np.ones(np.sum(~mask), dtype=bool))
                 name_idx_data.append(idx[~mask])
         append_cat = pd.DataFrame(
-            columns=["ra", "dec", "healpix"] + names + [f"{name}_idx" for name in names]
-            data=[cat.loc[~mask, col] for col in ["ra", "dec", "healpix"]] + name_data + name_idx_data 
+            columns=["ra", "dec", "healpix"] + names + [f"{name}_idx" for name in names],
+            data=np.stack(
+                [cat.loc[~mask, col] for col in ["ra", "dec", "healpix"]]
+                + name_data
+                + name_idx_data
+            ).T,
         )
 
         master_cat = pd.concat([master_cat, append_cat], ignore_index=True)
-
+        print("master cat size: ", master_cat.shape)
+    master_cat["ra"] = master_cat["ra"].astype(float)
+    master_cat["dec"] = master_cat["dec"].astype(float)
+    master_cat["healpix"] = master_cat["healpix"].astype(int)
+    for name in names:
+        master_cat[name] = master_cat[name].astype(bool)
+        master_cat[f"{name}_idx"] = master_cat[f"{name}_idx"].astype(int)
     return master_cat
