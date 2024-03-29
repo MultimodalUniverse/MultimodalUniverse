2e8254079c07a4e32693368e14e1b7a07125c509
lhparker1 <liamholdenparker@gmail.com>
2024-03-29 10:55:24 -0400

    Updating dataset wrapper

--- baselines/photo_z/data_wrapper.py
+++ baselines/photo_z/data_wrapper.py
@@ -1,18 +1,18 @@
 import sys
-sys.path.append('../')  # TODO
+sys.path.append('../')
 import torch
-from datasets.arrow_dataset import Dataset as HF_Dataset  # for typing etc
-from pytorch_lightning import LightningDataModule
+from lightning import LightningDataModule
 from torch.utils.data import DataLoader
-from dataset_utils import compute_dataset_statistics, normalize_sample, get_nested
-
+from datasets.arrow_dataset import Dataset as HF_Dataset
+from dataset_utils import split_dataset, compute_dataset_statistics, normalize_sample, get_nested
+from typing import Any
 
 class DatasetWrapper(LightningDataModule):
     def __init__(self, 
                  train_dataset: HF_Dataset, 
                  test_dataset: HF_Dataset,
-                 feature_flag: str = 'images_by_batch.array',
-                 label_flag: str = 'label',
+                 feature_flag: str = 'image',
+                 label_flag: str = 'z',
                  feature_dynamic_range: bool = True,
                  feature_z_score: bool = True,
                  label_dynamic_range: bool = False,
@@ -26,22 +26,6 @@ class DatasetWrapper(LightningDataModule):
         """
         Initializes the data module with a dataset that is already loaded, setting up parameters
         for data processing and batch loading.
-
-        Parameters:
-        - train_dataset (Dataset): The pre-loaded dataset, expected to be a torch.utils.data.Dataset with images in size B x C x H x W.
-        - test_dataset (Dataset): The pre-loaded dataset, expected to be a torch.utils.data.Dataset with images in size B x C x H x W.
-        - feature_flag (str): The key in the dataset corresponding to the image data.
-        - label_flag (str): The key in the dataset corresponding to the redshift data.
-        - feature_dynamic_range (bool): Flag indicating whether dynamic range compression should be applied.
-        - feature_z_score (bool): Flag indicating whether z-score normalization should be applied.
-        - label_dynamic_range (bool): Flag indicating whether dynamic range compression should be applied.
-        - label_z_score (bool): Flag indicating whether z-score normalization should be applied.
-
-        - batch_size (int): The size of each data batch for loading.
-        - num_workers (int): Number of subprocesses to use for data loading.
-        - val_size (float): The proportion of the dataset to reserve for validation.
-        - loading (str): Approach for loading the dataset ('full' or 'iterated').
-
         """
 
         self._train_dataset = train_dataset
@@ -57,10 +41,10 @@ class DatasetWrapper(LightningDataModule):
 
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.prepare_data_per_node = False
+        self.val_size = val_size
         self.loading = loading
 
-        self.val_size = self.val_size
+        self.prepare_data_per_node = False
 
     def prepare_data(self):
         # Compute the dataset statistics
@@ -81,21 +65,13 @@ class DatasetWrapper(LightningDataModule):
         batch = torch.utils.data.default_collate(batch)
         x = normalize_sample(get_nested(batch, self.feature_flag), self.feature_mean, self.feature_std, dynamic_range=self.feature_dynamic_range, z_score=self.feature_z_score) # dynamic range compression and z-score normalization
         y = normalize_sample(get_nested(batch, self.label_flag), self.label_mean, self.label_std, dynamic_range=self.label_dynamic_range, z_score=self.label_z_score)
-        
-        if not isinstance(x, torch.Tensor):
-            x = torch.tensor(x)
-        if not isinstance(y, torch.Tensor):
-            y = torch.tensor(y)
-
         return x, y
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=self.collate_fn)
-    
+
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=self.collate_fn)
-    
+
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=self.collate_fn)
-    
-    
