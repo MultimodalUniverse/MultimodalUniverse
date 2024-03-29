0e8c071e580939881404c118ca460fc874044db7
havok2063 <havok2063@hotmail.com>
2024-03-29 12:55:07 -0400

    tweaking cli and dataset output

--- scripts/manga/manga.py
+++ scripts/manga/manga.py
@@ -41,7 +41,7 @@ class MaNGA(datasets.GeneratorBasedBuilder):
     BUILDER_CONFIGS = [
         datasets.BuilderConfig(name="manga",
                                version=VERSION,
-                               data_files=DataFilesPatternsDict.from_patterns({'train': ['out/healpix=*/*.hdf5']}),
+                               data_files=DataFilesPatternsDict.from_patterns({'train': ['out/manga/healpix=*/*.hdf5']}),
                                description="SDSS MaNGA IFU log data cubes"),
     ]
 
