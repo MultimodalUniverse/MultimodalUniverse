772dfaa4cb8c58b441532fc9b0d064041aa845ad
Jeff Shen <shenjeff@princeton.edu>
2024-04-04 12:05:45 -0400

    more verbose testing script

--- scripts/gaia/test_load.py
+++ scripts/gaia/test_load.py
@@ -10,8 +10,19 @@ ds = datasets.load_dataset(
 
 batch = next(iter(ds))
 
+print("testing spectral coefficients")
+
 assert batch["spectral_coefficients"]["coeff"].size == 110
 assert batch["spectral_coefficients"]["coeff_error"].size == 110
 
+print("testing astrometry")
+
 for k in ["ra", "dec", "pmra", "pmdec", "parallax"]:
     assert k in batch["astrometry"].keys()
+
+print("testing photometry")
+
+for k in ["bp_rp", "phot_g_mean_mag", "phot_bp_mean_mag", "phot_rp_mean_mag"]:
+    assert k in batch["photometry"].keys()
+
+print("Test passed!")
