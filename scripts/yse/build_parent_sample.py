f18f9be97a413da3de58f1767aed5940d1738670
Micah Bowles <micah.r.bowles@gmail.com>
2024-04-09 13:29:43 +0100

    yse files

---
+++ scripts/yse/build_parent_sample.py
@@ -0,0 +1,166 @@
+import argparse
+import h5py
+import numpy as np
+import os
+import shutil
+import sncosmo
+
+def get_str_dtype(arr):
+    str_max_len = int(np.char.str_len(arr).max())
+    return h5py.string_dtype(encoding='utf-8', length=str_max_len)
+
+def convert_dtype(arr):
+    if np.issubdtype(arr.dtype, np.floating):
+        dtype = np.float32
+    elif np.issubdtype(arr.dtype, np.str_):
+        dtype = get_str_dtype(arr)
+    else:
+        dtype = arr.dtype
+    return arr.astype(dtype)
+
+def main(args):
+    # Retrieve file paths
+    file_dir = args.yse_data_path
+    files = os.listdir(file_dir)
+    num_examples = len(files)
+
+    # Set which keys will be ignored when loading/converting/saving the data
+    ignored_keys = {
+        #'PARSNIP_S1',
+        #'PARSNIP_S2',
+        #'PARSNIP_S3',
+        #'SET_ZTF_FP',
+        #'processing',
+        #'SEARCH_PEAKFLT',
+        'END',
+        'FIELD',
+        'FLAG',
+    }
+
+    # Load example data to determine keys in the dataset
+    example_metadata, example_data = sncosmo.read_snana_ascii(os.path.join(file_dir, files[0]), default_tablename='OBS')
+    example_data = example_data['OBS']
+    keys_metadata = list(example_metadata.keys())
+    keys_data = list(example_data.keys())
+
+    # Remove ignored keys
+    for key in keys_metadata:
+        if key in ignored_keys:
+            keys_metadata.remove(key)
+    for key in keys_data:
+        if key in ignored_keys:
+            keys_data.remove(key)
+
+    # Define keys that comprise the standard lightcurve data
+    keys_lightcurve = ['MJD', 'FLUXCAL', 'FLUXCALERR']
+
+    # Initialize dictionaries to store data and metadata
+    data = dict(zip(keys_data, ([] for _ in keys_data)))
+    metadata = dict(zip(keys_metadata, ([] for _ in keys_metadata)))
+
+    for file in files:
+        # Load data and metadata from snana files using functionality from sncosmo
+        metadata_, data_ = sncosmo.read_snana_ascii(os.path.join(file_dir, file), default_tablename='OBS')
+        data_ = data_['OBS']
+        # Iterate over keys and append data to the corresponding list in data / metadata dicts
+        for key in keys_data:
+            if key in data_.keys(): data[key].append(data_[key].data)
+            else: data[key].append(np.full(0, np.nan))
+            # The data are astropy columns wrapping numpy arrays which are accessed via .data
+        for key in keys_metadata:
+            if key in metadata_.keys(): metadata[key].append(metadata_[key])
+            else: metadata[key].append(np.nan)
+
+    # Create an array of all bands in the dataset
+    all_bands = np.unique(np.concatenate(data['FLT']))
+
+    # Determine the length of the longest light curve (in a specific band) in the dataset
+    max_length = 0
+    for i in range(num_examples):
+        _, count = np.unique(data['FLT'][i], return_counts=True)
+        max_length = max(max_length, count.max(initial=0))
+
+    # Remove band from field_data as the timeseries will be arranged by band
+    keys_data.remove('FLT')
+
+    # Initialize lists to store lightcurve data
+    lightcurve = []
+    lightcurve_additional = []
+
+    for i in range(num_examples):
+        # Create masks to select data from each timeseries by band
+        mask = np.expand_dims(all_bands, 1) == data['FLT'][i]
+        data_block = []  # Stores all timeseries in lightcurve for this example
+        data_block_additional = []  # Stores all timeseries in lightcurve_additional for this example
+        for key in keys_data:
+            d = []  # Stores a particular timeseries (corresponding to the key) for each band
+            for j in range(len(all_bands)):
+                d_ = data[key][i][mask[j]]  # Select samples from timeseries for a specific band
+                d_ = np.pad(d_,
+                            (0, max_length - len(d_)),
+                            mode='constant',
+                            constant_values=-99 if key == 'MJD' else 0
+                            )  # Pad band timeseries to the length of the longest timeseries
+                d.append(d_)
+            # Append complete timeseries organised by band to relevant list storing lightcurve(_additional)
+            if key in keys_lightcurve:
+                data_block.append(np.expand_dims(np.array(d), 1))
+            else:
+                data_block_additional.append(np.expand_dims(np.array(d), 1))
+        # Expand dims of data_block(_additional) in preparation to concatenate over examples along dim 1
+        data_block = np.concatenate(data_block, 1)
+        data_block_additional = np.concatenate(data_block_additional, 1)
+        # Append complete lightcurve(_additional) for this example to the relevant list storing all examples
+        lightcurve.append(data_block)
+        lightcurve_additional.append(data_block_additional)
+
+    # Convert lightcurve (core and additional) data to numpy array
+    lightcurve = np.array(lightcurve, dtype=np.float32)
+    lightcurve_additional = np.array(lightcurve_additional, dtype=np.float32)
+
+    # Add numeric object_id to metadata (integer for each example in order of reading files)
+    metadata['object_id'] = np.arange(1, num_examples + 1)
+
+    # Convert metadata to numpy arrays
+    for key in keys_metadata:
+        metadata[key] = np.array(metadata[key])
+
+    # Convert keys to standard names
+    keys_all = keys_metadata + keys_data
+    name_conversion = dict(zip(keys_all, keys_all))
+    name_conversion.update({
+        #'SNID': 'object_id',
+        'RA': 'ra',
+        'DECL': 'dec',
+        #'MJD': 'time',
+        #'FLUXCAL': 'flux',
+        #'FLUXCALERR': 'flux_err',
+        #'FLT': 'band',
+        #'FLAG': 'quality_mask',
+    })
+    #keys_data = [name_conversion[key] for key in keys_data]
+    #keys_metadata = [name_conversion[key] for key in keys_metadata]
+
+    # Save file as hdf5
+    with h5py.File(os.path.join(args.output_dir, 'yse.h5'), 'w') as hdf5_file:
+        # Save metadata
+        for key in keys_metadata:
+            hdf5_file.create_dataset(name_conversion[key], data=convert_dtype(metadata[key]))
+        # Save bands
+        hdf5_file.create_dataset('bands', data=convert_dtype(all_bands))
+        # Save core timeseries
+        hdf5_file.create_dataset('lightcurve', data=lightcurve)
+        # Save additional timeseries
+        hdf5_file.create_dataset('lightcurve_additional', data=lightcurve_additional)
+
+    # Remove original data downloaded from Zenodo
+    if True:
+        shutil.rmtree(args.yse_data_path)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Extract YSE data and convert to standard time-series data format.')
+    parser.add_argument('yse_data_path', type=str, help='Path to the local copy of the YSE DR1 data')
+    parser.add_argument('output_dir', type=str, help='Path to the output directory')
+    args = parser.parse_args()
+
+    main(args)
---
+++ scripts/yse/yse.py
@@ -0,0 +1,172 @@
+# Copyright 2020 The HuggingFace Datasets Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import datasets
+from datasets import Features, Value, Sequence
+from datasets.data_files import DataFilesPatternsDict
+import itertools
+import h5py
+import numpy as np
+
+# TODO: Add BibTeX citation
+# Find for instance the citation on arxiv or on the dataset repo/website
+_CITATION = """\
+@InProceedings{huggingface:dataset,
+title = {A great new dataset},
+author={huggingface, Inc.
+},
+year={2020}
+}
+"""
+
+# TODO: Add description of the dataset here
+# You can copy an official description
+_DESCRIPTION = """\
+Time-series dataset from the Young Supernova Experiment Data Release 1 (YSE DR1).
+"""
+
+# TODO: Add a link to an official homepage for the dataset here
+_HOMEPAGE = "https://zenodo.org/records/7317476"
+
+# TODO: Add the licence for the dataset here if you can find it
+_LICENSE = ""
+
+_VERSION = "0.0.1"
+
+_STR_FEATURES = [
+    "object_id",
+    "spec_class",
+    "bands",
+]
+
+_FLOAT_FEATURES = ["ra", "dec", "redshift", "host_log_mass"]
+
+
+class YSEDR1(datasets.GeneratorBasedBuilder):
+    """"""
+
+    VERSION = _VERSION
+
+    BUILDER_CONFIGS = [
+        datasets.BuilderConfig(
+            name="yse_dr1",
+            version=VERSION,
+            data_files=DataFilesPatternsDict.from_patterns({"train": ["*/*.h5"]}),
+            description="Light curves from YSE DR1",
+        ),
+    ]
+
+    DEFAULT_CONFIG_NAME = "yse_dr1"
+
+    @classmethod
+    def _info(self):
+        """Defines the features available in this dataset."""
+        # Starting with all features common to light curve datasets
+        features = {
+            "band_idx": Sequence(Value("int32")),
+            "time": Sequence(Value("float32")),
+            "flux": Sequence(Value("float32")),
+            "flux_err": Sequence(Value("float32")),
+        }
+
+        # Adding all values from the catalog
+        for f in _FLOAT_FEATURES:
+            features[f] = Value("float32")
+        for f in _STR_FEATURES:
+            features[f] = Value("string")
+
+        return datasets.DatasetInfo(
+            # This is the description that will appear on the datasets page.
+            description=_DESCRIPTION,
+            # This defines the different columns of the dataset and their types
+            features=Features(features),
+            # Homepage of the dataset for documentation
+            homepage=_HOMEPAGE,
+            # License for the dataset if available
+            license=_LICENSE,
+            # Citation for the dataset
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """We handle string, list and dicts in datafiles"""
+        if not self.config.data_files:
+            raise ValueError(
+                f"At least one data file must be specified, but got data_files={self.config.data_files}"
+            )
+        data_files = dl_manager.download_and_extract(self.config.data_files)
+        if isinstance(data_files, (str, list, tuple)):
+            files = data_files
+            if isinstance(files, str):
+                files = [files]
+            # Use `dl_manager.iter_files` to skip hidden files in an extracted archive
+            files = [dl_manager.iter_files(file) for file in files]
+            return [
+                datasets.SplitGenerator(
+                    name=datasets.Split.TRAIN, gen_kwargs={"files": files}
+                )
+            ]
+        splits = []
+        for split_name, files in data_files.items():
+            if isinstance(files, str):
+                files = [files]
+            # Use `dl_manager.iter_files` to skip hidden files in an extracted archive
+            files = [dl_manager.iter_files(file) for file in files]
+            splits.append(
+                datasets.SplitGenerator(name=split_name, gen_kwargs={"files": files})
+            )
+        return splits
+
+    def _generate_examples(self, files, object_ids=None):
+        """Yields examples as (key, example) tuples."""
+        for file_number, file in enumerate(itertools.chain.from_iterable(files)):
+            with h5py.File(file, "r") as data:
+                if object_ids is not None:
+                    keys = object_ids[file_number]
+                else:
+                    keys = data["object_id"]
+
+                # Preparing an index for fast searching through the catalog
+                sort_index = np.argsort(data["object_id"])
+                sorted_ids = data["object_id"][:][sort_index]
+
+                for k in keys:
+                    # Extract the indices of requested ids in the catalog
+                    i = sort_index[np.searchsorted(sorted_ids, k)]
+                    # Parse data
+                    idxs = np.arange(0, data["lightcurve"][i].shape[0])
+                    band_numbers = idxs.repeat(data["lightcurve"][i].shape[-1]).reshape(
+                        6, 1, 146
+                    )
+                    lightcurve_data = np.concatenate(
+                        [data["lightcurve"][i], band_numbers], axis=1
+                    )
+                    lightcurve_data = np.moveaxis(lightcurve_data, 0, 1).reshape(4, -1)
+                    example = {
+                        "band_idx": lightcurve_data[0],
+                        "time": lightcurve_data[1],
+                        "flux": lightcurve_data[2],
+                        "flux_err": lightcurve_data[3],
+                    }
+                    # Add remaining features
+                    for f in _FLOAT_FEATURES:
+                        example[f] = data[f][i].astype("float32")
+                    for f in _STR_FEATURES:
+                        # Add band names shared across dataset to each sample.
+                        # I can't see a better way to do this.
+                        if f == "bands":
+                            example[f] = np.asarray(data[f]).astype("str")
+                        else:
+                            example[f] = data[f][i].astype("str")
+
+                    yield str(data["object_id"][i]), example
