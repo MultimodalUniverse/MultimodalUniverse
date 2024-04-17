73e515d34503e80bdd5f1b5e5d56c372b51ee311
Benjamin Boyd <bmb41@calx015.ast.cam.ac.uk>
2024-04-17 18:20:09 +0100

    swift update bands

--- scripts/swift_sne_ia/build_parent_sample.py
+++ scripts/swift_sne_ia/build_parent_sample.py
@@ -60,8 +60,10 @@ def main(args):
         data_ = data_['OBS']
         # Iterate over keys and append data to the corresponding list in data / metadata dicts
         for key in keys_data:
-            if key in data_.keys(): data[key].append(data_[key].data)
-            else: data[key].append(np.full(0, np.nan))
+            if key in data_.keys(): 
+                data[key].append(data_[key].data)
+            else: 
+                data[key].append(np.full(0, np.nan))
             # The data are astropy columns wrapping numpy arrays which are accessed via .data
         metadata_['SNTYPE']="Ia"
         for key in keys_metadata:
@@ -161,7 +163,13 @@ def main(args):
             for key in keys_metadata:
                 hdf5_file.create_dataset(name_conversion[key], data=metadata[key][i])
             # Save bands
-            hdf5_file.create_dataset('bands', data=all_bands)
+            hdf5_file.create_dataset(
+                'bands', data=",".join(
+                    list(
+                        all_bands.squeeze().astype(str)
+                    )
+                )
+            )
             # Save timeseries
             for key in keys_data:
                 hdf5_file.create_dataset(name_conversion[key], data=data[key][i])
