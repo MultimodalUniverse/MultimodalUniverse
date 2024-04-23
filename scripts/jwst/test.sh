a3a43b2fd3788a741247a8d50da599f4c6a54e57
mhuertascompany <marc.huertas@obspm.fr>
2024-04-23 09:53:05 +0100

    test jwst

--- scripts/jwst/test.sh
+++ scripts/jwst/test.sh
@@ -1,55 +1,5 @@
 #!/bin/bash
 
-echo "Testing primer-uds"
-# Then build this parent sample for the primer field 
-if python build_parent_sample.py primer-uds --subsample tiny; then
-    echo "Build parent sample for deep field successful"
-else
-    echo "Build parent sample for deep field failed"
-    exit 1
-fi
-# Try to load the dataset with hugging face dataset
-if python -c "from datasets import load_dataset; dset = load_dataset('./jwst.py', 'primer-uds-tiny', trust_remote_code=True, split='train').with_format('numpy'); print(next(iter(dset)))"; then
-    echo "Load dataset for deep field successful"
-else
-    echo "Load dataset for deep field failed"
-    exit 1
-fi
-
-echo "Testing gdn"
-# First build the parent sample for the gdn field
-if python build_parent_sample.py gdn --subsample tiny; then
-    echo "Build parent sample for deep field successful"
-else
-    echo "Build parent sample for deep field failed"
-    exit 1
-fi
-# Try to load the dataset with hugging face dataset
-if python -c "from datasets import load_dataset; dset = load_dataset('./jwst.py', 'gdn-tiny', trust_remote_code=True, split='train').with_format('numpy'); print(next(iter(dset)))"; then
-    echo "Load dataset for deep field successful"
-else
-    echo "Load dataset for deep field failed"
-    exit 1
-fi
-
-echo "Testing gds"
-# Then build this parent sample for the gds field 
-if python build_parent_sample.py gds --subsample tiny; then
-    echo "Build parent sample for deep field successful"
-else
-    echo "Build parent sample for deep field failed"
-    exit 1
-fi
-
-# Try to load the dataset with hugging face dataset
-if python -c "from datasets import load_dataset; dset = load_dataset('./jwst.py', 'gds-tiny', trust_remote_code=True, split='train').with_format('numpy'); print(next(iter(dset)))"; then
-    echo "Load dataset for deep field successful"
-else
-    echo "Load dataset for deep field failed"
-    exit 1
-fi
-
-
 echo "Testing ngdeep"
 # Then build this parent sample for the ngdeep field 
 if python build_parent_sample.py ngdeep --subsample tiny; then
@@ -100,3 +50,54 @@ else
     echo "Load dataset for deep field failed"
     exit 1
 fi
+
+echo "Testing primer-uds"
+# Then build this parent sample for the primer field 
+if python build_parent_sample.py primer-uds --subsample tiny; then
+    echo "Build parent sample for deep field successful"
+else
+    echo "Build parent sample for deep field failed"
+    exit 1
+fi
+# Try to load the dataset with hugging face dataset
+if python -c "from datasets import load_dataset; dset = load_dataset('./jwst.py', 'primer-uds-tiny', trust_remote_code=True, split='train').with_format('numpy'); print(next(iter(dset)))"; then
+    echo "Load dataset for deep field successful"
+else
+    echo "Load dataset for deep field failed"
+    exit 1
+fi
+
+echo "Testing gdn"
+# First build the parent sample for the gdn field
+if python build_parent_sample.py gdn --subsample tiny; then
+    echo "Build parent sample for deep field successful"
+else
+    echo "Build parent sample for deep field failed"
+    exit 1
+fi
+# Try to load the dataset with hugging face dataset
+if python -c "from datasets import load_dataset; dset = load_dataset('./jwst.py', 'gdn-tiny', trust_remote_code=True, split='train').with_format('numpy'); print(next(iter(dset)))"; then
+    echo "Load dataset for deep field successful"
+else
+    echo "Load dataset for deep field failed"
+    exit 1
+fi
+
+echo "Testing gds"
+# Then build this parent sample for the gds field 
+if python build_parent_sample.py gds --subsample tiny; then
+    echo "Build parent sample for deep field successful"
+else
+    echo "Build parent sample for deep field failed"
+    exit 1
+fi
+
+# Try to load the dataset with hugging face dataset
+if python -c "from datasets import load_dataset; dset = load_dataset('./jwst.py', 'gds-tiny', trust_remote_code=True, split='train').with_format('numpy'); print(next(iter(dset)))"; then
+    echo "Load dataset for deep field successful"
+else
+    echo "Load dataset for deep field failed"
+    exit 1
+fi
+
+
