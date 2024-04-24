aea1f989932fdcfe9bea58cc06b3741e123caaee
havok2063 <havok2063@hotmail.com>
2024-04-24 05:00:29 +0200

    adding test script

---
+++ scripts/manga/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install the sdss-access tool
+pip install sdss_access
+
+# download a small sample
+python access_transfer.py --limit --destination_path .
+
+# build the parent sample
+python build_parent_sample.py --manga_data_path . --output_dir out --num_processes 1
+
+# try to load the dataset
+python -c "from datasets import load_dataset; dset = load_dataset('./manga.py').with_format('numpy'); print(next(iter(dset)))";
