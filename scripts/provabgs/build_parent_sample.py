f2c31ae469b152e71a2d17bd440a6fe9a2c2f326
lhparker1 <liamholdenparker@gmail.com>
2024-04-25 10:40:55 -0400

    typing for parent sample

--- scripts/provabgs/build_parent_sample.py
+++ scripts/provabgs/build_parent_sample.py
@@ -5,15 +5,18 @@ import os
 from tqdm import tqdm
 import argparse
 from multiprocessing import Pool
+from jaxtyping import Str
 import h5py
 
-def save_in_standard_format(input_path, output_dir):
+def save_in_standard_format(input_path: Str, output_dir: Str):
     """Save the input HDF5 file in the standard format for the HEALPix-based dataset."""
     data = Table.read(input_path)
 
+    # Get the RA and DEC columns
     ra = data['RA']
     dec = data['DEC']
 
+    # Convert the RA and DEC to HEALPix indices and find the unique indices
     healpix_indices = hp.ang2pix(16, ra, dec, lonlat=True, nest=True)
     unique_indices = np.unique(healpix_indices)
 
@@ -23,11 +26,10 @@ def save_in_standard_format(input_path, output_dir):
     keys.remove('RA')
     keys.remove('DEC')
 
+    # Group the data by HEALPix index and save it in the standard format
     for index in tqdm(unique_indices, desc="Processing HEALPix indices"):
         mask = healpix_indices == index
-
         grouped_data = data[mask]
-        
         output_subdir = os.path.join(output_dir, f'healpix={index}')
 
         if not os.path.exists(output_subdir):
@@ -44,6 +46,7 @@ def save_in_standard_format(input_path, output_dir):
             output_file.create_dataset('healpix', data=np.full(grouped_data['TARGETID'].shape, index))
 
 def main(args):
+    """Main function to convert PROVABGS HDF5 file to the standard format for the HEALPix-based dataset."""
     output_dir = os.path.join(args.output_dir, 'datafiles')
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
