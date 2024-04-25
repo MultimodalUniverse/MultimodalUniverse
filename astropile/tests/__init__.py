ab81a3618c77711fbc0a74f8f03aa9dc9a590435
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2024-04-25 14:39:57 +0200

    [#56] Edit skipping test management

--- astropile/tests/__init__.py
+++ astropile/tests/__init__.py
@@ -4,5 +4,5 @@ import pytest
 
 
 def mark_dataset_test() -> pytest.MarkDecorator:
-    mark_name = osp.basename(osp.dirname(inspect.stack()[1].filename))
-    return getattr(pytest.mark, mark_name)
+    dataset_name = osp.basename(osp.dirname(inspect.stack()[1].filename))
+    return pytest.mark.dataset(dataset_name)
--- conftest.py
+++ conftest.py
@@ -1,9 +1,35 @@
+import pytest
 import os
 
 
-def pytest_configure(config):
+def pytest_addoption(parser: pytest.Parser):
+    parser.addoption(
+        "--diff",
+        dest="diff",
+        nargs="*",
+        help="Modified files to be considered to skip the tests.",
+    )
+
+
+def pytest_configure(config: pytest.Config):
     # Register additional markers to run dataset tests selectively
-    for dir in os.listdir("scripts"):
-        config.addinivalue_line(
-            "markers", f"{dir}: mark test to run only {dir} dataset tests."
+    config.addinivalue_line(
+        "markers", "dataset(name): mark test to run only dataset tests."
+    )
+
+
+def pytest_runtest_setup(item: pytest.Item):
+    dataset_mark = [mark.args[0] for mark in item.iter_markers(name="dataset")]
+    modified_files = item.config.getoption("--diff")
+    if dataset_mark and modified_files:
+        assert (
+            len(dataset_mark) == 1
+        ), f"Test should have only one dataset marker but got {dataset_mark}"
+        dataset_mark = dataset_mark[0]
+        modified_datasets = set(
+            os.path.basename(os.path.dirname(path))
+            for path in modified_files
+            if "scripts" in path
         )
+        if dataset_mark not in modified_datasets:
+            pytest.skip("Dataset has not been modified.")
