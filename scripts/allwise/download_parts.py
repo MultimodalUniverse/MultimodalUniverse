f9982560f4386ed8b57d665ffe594301c0e972a2
Jeff Shen <shenjeff@princeton.edu>
2024-04-26 00:11:34 -0400

    remove aria2 option

--- scripts/allwise/download_parts.py
+++ scripts/allwise/download_parts.py
@@ -8,7 +8,10 @@ from tqdm.contrib.concurrent import process_map
 
 def _download_file(f, args):
     f = f.strip()
-    savename = f"{args.output_dir}/{f.split('/')[-1]}"
+    filename = f.split("wise-allwise.parquet/")[-1]
+    filedir = "/".join(filename.split("/")[:-1])
+    os.makedirs(f"{args.output_dir}/{filedir}", exist_ok=True)
+    savename = f"{args.output_dir}/{filename}"
     if not os.path.exists(savename):
         urllib.request.urlretrieve(f, savename)
 
@@ -16,37 +19,22 @@ def _download_file(f, args):
 def main(args):
     os.makedirs(args.output_dir, exist_ok=True)
 
-    if not args.aria2:
-        with open("file_list.txt") as f:
-            files = f.readlines()
+    with open("file_list.txt") as f:
+        files = f.readlines()
 
-        if args.tiny:
-            files = files[:3]
+    if args.tiny:
+        files = files[:3]
 
-        _download = partial(_download_file, args=args)
+    _download = partial(_download_file, args=args)
 
-        process_map(_download, files, max_workers=16, chunksize=1)
-
-    else:
-        if args.tiny:
-            with open("file_list.txt") as f:
-                files = f.readlines()[:3]
-            files = " ".join([f'"{f.strip()}"' for f in files])
-
-            os.system(f"aria2c -j8 -x8 -s8 -c -d {args.output_dir} -Z {files}")
-
-        else:
-            os.system(f"aria2c -j16 -x16 -s16 -c -i file_list.txt -d {args.output_dir}")
+    process_map(_download, files, max_workers=16, chunksize=1)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--aria2", help="use aria2c for downloading", action="store_true"
-    )
-    parser.add_argument(
         "--tiny",
-        help="download a single source and coeff file only",
+        help="download a single file only",
         action="store_true",
     )
     parser.add_argument(
