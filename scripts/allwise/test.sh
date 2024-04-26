2e99118900138137a4e014008c3326dd3ca579eb
Jeff Shen <shenjeff@princeton.edu>
2024-04-26 03:21:07 -0400

    write test script

--- scripts/allwise/test.sh
+++ scripts/allwise/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo =============
-echo GAIA DR3 TEST
+echo AllWISE TEST
 echo =============
 
 echo ========================
@@ -9,22 +9,21 @@ echo DOWNLOADING TINY DATASET
 echo ========================
 
 # download files
-python3 download_parts.py --tiny --output_dir .
-
-echo ==================
-echo MERGING HDF5 FILES
-echo ==================
-
-# merge files
-python3 merge_parts.py --input_dir . --output_file merged.hdf5
+python3 download_parts.py --tiny --output_dir _allwise
 
 echo ===================
 echo HEALPIXIFYING FILES
 echo ===================
 
 # healpixify 
-mkdir gaia
-python3 healpixify.py --input_file merged.hdf5 --output_dir . --nside 8
+python3 healpixify.py --nside 16 --input_dir _allwise --output_dir _allwise_hp
+
+echo ==================
+echo CONVERTING TO HDF5 
+echo ==================
+
+# convert
+python3 to_hdf5.py --data_dir _allwise_hp --output_dir allwise
 
 echo ==================
 echo TESTING HF LOADING
@@ -32,3 +31,7 @@ echo ==================
 
 # test loading
 python3 test_load.py
+
+echo ============
+echo TEST PASSED! 
+echo ============
