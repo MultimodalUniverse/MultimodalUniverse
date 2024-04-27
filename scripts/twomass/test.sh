43d5788be48a34423afcb2145246bf58ee4bbf47
Jeff Shen <shenjeff@princeton.edu>
2024-04-27 14:22:42 -0400

    update test script and paths

--- scripts/twomass/test.sh
+++ scripts/twomass/test.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-echo =============
-echo AllWISE TEST
-echo =============
+echo ==========
+echo 2MASS TEST
+echo ==========
 
 echo ========================
 echo DOWNLOADING TINY DATASET
 echo ========================
 
 # download files
-python3 download_parts.py --tiny --output_dir _allwise
+python3 download_parts.py --tiny --output_dir _2mass/psc
 
-echo ===================
-echo HEALPIXIFYING FILES
-echo ===================
+echo =====================================
+echo PREPARING PARTITIONED PARQUET DATASET
+echo =====================================
 
 # healpixify 
-python3 healpixify.py --nside 16 --input_dir _allwise --output_dir _allwise_hp
+python3 to_parquet.py --nside 16 --input_dir _2mass/psc --output_dir _2mass_pq/psc --file_prefix psc
 
 echo ==================
 echo CONVERTING TO HDF5 
 echo ==================
 
 # convert
-python3 to_hdf5.py --data_dir _allwise_hp --output_dir allwise
+python3 to_hdf5.py --data_dir _2mass_pq/psc --output_dir 2mass/psc
 
 echo ==================
 echo TESTING HF LOADING
--- scripts/twomass/to_parquet.py
+++ scripts/twomass/to_parquet.py
@@ -1,6 +1,7 @@
 import argparse
 from functools import partial
 import glob
+import os
 
 import healpy as hp
 import pyarrow as pa
@@ -34,10 +35,13 @@ if __name__ == "__main__":
     parser.add_argument("--data_dir", type=str, required=True)
     parser.add_argument("--nside", type=int, default=16)
     parser.add_argument("--output_dir", type=str, required=True)
+    parser.add_argument("--file_prefix", type=str, required=True)
     args = parser.parse_args()
 
-    filenames = glob.glob(f"{args.data_dir}/**/psc*")
+    filenames = glob.glob(f"{args.data_dir}/{args.file_prefix}*")
 
     results = process_map(partial(read_table, args=args), filenames)
     table = pa.concat_tables(results)  # , promote_options="permissive")
+
+    os.makedirs(args.output_dir, exist_ok=True)
     pq.write_to_dataset(table, args.output_dir, partition_cols=["healpix"])
