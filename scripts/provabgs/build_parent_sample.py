b3d341a8fe966af60be01bf62b79461256db7973
lhparker1 <liamholdenparker@gmail.com>
2024-05-02 18:16:22 -0400

    update with provabgs fitting

--- scripts/provabgs/build_parent_sample.py
+++ scripts/provabgs/build_parent_sample.py
@@ -8,8 +8,10 @@ from multiprocessing import Pool
 from jaxtyping import Str
 import h5py
 
+from provabgs import Models
 
-provabgs_file = 'https://data.desi.lbl.gov/public/edr/vac/edr/provabgs/v1.0/BGS_ANY_full.provabgs.sv3.v0.hdf5   '
+provabgs_file = 'https://data.desi.lbl.gov/public/edr/vac/edr/provabgs/v1.0/BGS_ANY_full.provabgs.sv3.v0.hdf5'
+
 
 def download_data(save_path: str):
     """Download the PROVABGS data from the web and save it to the specified directory."""
@@ -27,9 +29,47 @@ def download_data(save_path: str):
         print("PROVABGS data already exists!")
 
 
+def _get_best_fit(provabgs: Table):
+    """Get the best fit model for each galaxy."""
+    m_nmf = Models.NMF(burst=True, emulator=True)
+
+    # Filter out galaxies with no best fit model
+    provabgs = provabgs[(provabgs['PROVABGS_LOGMSTAR_BF'] > 0) *
+                   (provabgs['MAG_G'] > 0) *
+                   (provabgs['MAG_R'] > 0) *
+                   (provabgs['MAG_Z'] > 0)]
+
+    # Get the thetas and redshifts for each galaxy
+    thetas = provabgs['PROVABGS_THETA_BF'][:, :12]
+    zreds = provabgs['Z_HP']
+
+    Z_mw    = [] # Stellar Metallicitiy
+    tage_mw = [] # Age
+    avg_sfr = [] # Star-Forming Region
+
+    print("Calculating properties using the PROVABGS model...")
+    for i in tqdm(range(len(thetas))):
+        theta = thetas[i]
+        zred = zreds[i]
+        
+        # Calculate properties using the PROVABGS model
+        Z_mw.append(m_nmf.Z_MW(theta, zred=zred))
+        tage_mw.append(m_nmf.tage_MW(theta, zred=zred))
+        avg_sfr.append(m_nmf.avgSFR(theta, zred=zred))
+
+    # Add the properties to the table
+    provabgs['Z_MW'] = np.array(Z_mw)
+    provabgs['TAGE_MW'] = np.array(tage_mw)
+    provabgs['AVG_SFR'] = np.array(avg_sfr)
+    return provabgs
+
+
 def save_in_standard_format(input_path: Str, output_dir: Str):
     """Save the input HDF5 file in the standard format for the HEALPix-based dataset."""
     data = Table.read(input_path)
+
+    # Get the best fit model for each galaxy
+    data = _get_best_fit(data)
 
     # Get the RA and DEC columns
     ra = data['RA']
