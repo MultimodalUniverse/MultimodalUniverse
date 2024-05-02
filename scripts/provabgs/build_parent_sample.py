0007b89200e060c3cdb4760bbe2d5dccb4b5f5c3
lhparker1 <liamholdenparker@gmail.com>
2024-05-02 18:25:38 -0400

    small typos

--- scripts/provabgs/build_parent_sample.py
+++ scripts/provabgs/build_parent_sample.py
@@ -5,10 +5,9 @@ import os
 from tqdm import tqdm
 import argparse
 from multiprocessing import Pool
-from jaxtyping import Str
 import h5py
 
-from provabgs import Models
+from provabgs import models as Models
 
 provabgs_file = 'https://data.desi.lbl.gov/public/edr/vac/edr/provabgs/v1.0/BGS_ANY_full.provabgs.sv3.v0.hdf5'
 
@@ -64,7 +63,7 @@ def _get_best_fit(provabgs: Table):
     return provabgs
 
 
-def save_in_standard_format(input_path: Str, output_dir: Str):
+def save_in_standard_format(input_path: str, output_dir: str):
     """Save the input HDF5 file in the standard format for the HEALPix-based dataset."""
     data = Table.read(input_path)
 
@@ -109,11 +108,16 @@ def main(args):
     output_dir = os.path.join(args.output_dir, 'datafiles')
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+
+    # Download the data if it does not already exist
+    download_data(args.input_path)
+
+    # Save the data in the standard format
     save_in_standard_format(args.input_path, output_dir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Convert an HDF5 file to the standard format for the HEALPix-based dataset")
-    parser.add_argument('input_path', type=str, help="Path to the input HDF5 file")
+    parser.add_argument('input_path', type=str, help="Path to the input HDF5 file. If the file does not exist, it will be downloaded.")
     parser.add_argument('output_dir', type=str, help="Path to the output directory")
     args = parser.parse_args()
     main(args)
