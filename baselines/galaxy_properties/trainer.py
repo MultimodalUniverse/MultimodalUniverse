37fe3b000e37769f49a10c9677bf5c9dd13e6ec2
lhparker1 <liamholdenparker@gmail.com>
2024-05-23 12:19:16 -0400

    typo

--- baselines/galaxy_properties/trainer.py
+++ baselines/galaxy_properties/trainer.py
@@ -1,10 +1,10 @@
 from lightning.pytorch.cli import ArgsType, LightningCLI
 
-def photo_z_cli(args: ArgsType = None, run: bool = True):
+def provabgs_cli(args: ArgsType = None, run: bool = True):
     return LightningCLI(
         args=args,
         run=run,
     )
 
 if __name__ == "__main__":
-    photo_z_cli(run=True)
+    provabgs_cli(run=True)
