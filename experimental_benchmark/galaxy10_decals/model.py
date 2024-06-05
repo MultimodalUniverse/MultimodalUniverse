bed866c61a132c052813000ecee7a5e6ba4341f9
lhparker1 <liamholdenparker@gmail.com>
2024-06-05 17:13:41 -0400

    small conv model

--- experimental_benchmark/galaxy10_decals/model.py
+++ experimental_benchmark/galaxy10_decals/model.py
@@ -26,15 +26,6 @@ class GZ10Model(L.LightningModule):
         # Set up metrics
         self.accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=num_classes, top_k=top_k)
 
-        # Set up transforms
-        self.transforms = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomVerticalFlip(),
-            transforms.RandomAffine(0, translate=(0.1, 0.1)),
-            transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
-            transforms.GaussianBlur(3),
-        ])
-
     def forward(self, x):
         return self.model(x)
 
@@ -104,3 +95,64 @@ class ResNet18(GZ10Model):
                 input_channels, 64, kernel_size=7, stride=2, padding=3, bias=False
         )
         self.model.fc = nn.Linear(512, num_classes)  
+
+        # Set up transforms
+        self.transforms = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomAffine(0, translate=(0.2, 0.2)),
+            transforms.RandomRotation(180),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+            transforms.GaussianBlur(3),
+        ])
+
+
+class SmallConvModel(GZ10Model):
+    """Small convolutional model for galaxy property estimation"""
+    def __init__(
+        self,
+        input_channels: int = 3,
+        num_classes: int = 10,
+        lr: float = 1e-4,
+        label_smoothing: float = 0.,
+        weight: torch.Tensor = None,
+        reduction: str = 'mean',
+        top_k: int = 1,
+    ):
+        super().__init__(
+            input_channels=input_channels,
+            num_classes=num_classes,
+            lr=lr,
+            label_smoothing=label_smoothing,
+            weight=weight,
+            reduction=reduction,
+            top_k=top_k
+        )
+        self.save_hyperparameters()
+
+        # Set up the model, input image size is 256x256
+        self.model = nn.Sequential(
+            nn.Conv2d(input_channels, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Flatten(),
+            nn.Linear(512 * 8 * 8, num_classes),
+        )
+
+        # Set up transforms
+        self.transforms = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+        ])
