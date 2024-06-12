363c40b8b8ad74411f50bf8df435621fa2b98f4d
Aaron Do <ajmd6@cam.ac.uk>
2024-06-12 09:44:15 +0100

    Added homepage to cfa3.py. Trimmed homepages in cfa4.py cfa_snII.py to point at the pages holding the data files rather than the data files.

--- scripts/cfa/cfa3.py
+++ scripts/cfa/cfa3.py
@@ -43,7 +43,7 @@ _DESCRIPTION = """\
 Time-series dataset from the Center for Astronomy 3 Data Release.
 """
 
-_HOMEPAGE = ""
+_HOMEPAGE = "https://lweb.cfa.harvard.edu/supernova/CfA3/"
 
 _LICENSE = "CC BY 3.0"
 
