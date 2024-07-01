0ddc810b02f0113ba0247475b644584138035159
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2024-07-01 13:15:20 +0200

    [#110] Fix sanity check script

--- scripts/sanity_check.py
+++ scripts/sanity_check.py
@@ -21,19 +21,19 @@ if __name__ == "__main__":
     parser.add_argument("--n_proc", type=int, default=1, help="Number of processes")
     parser.add_argument("--chunks", type=int, default=1, help="Chunk size per process")
     args = parser.parse_args()
-    root = args.sample_paths
+    root = args.sample_path
     n_proc = args.n_proc
     chunks = args.chunks
     path = f"{root}/*/*/*.hdf5"
     files = glob(path)
+    n_problematic_files = 0
     with multiprocessing.Pool(n_proc) as pool:
         results = tqdm(
             pool.imap_unordered(check_file, files, chunksize=chunks), total=len(files)
         )
-    n_problematic_files = 0
-    for r in results:
-        if r is not None:
-            filename, error = r
-            print(f"Fail to load {filename} due to error {error}")
-            n_problematic_files += 1
+        for r in results:
+            if r is not None:
+                filename, error = r
+                print(f"Fail to load {filename} due to error {error}")
+                n_problematic_files += 1
     print(f"Failed to load {n_problematic_files} files out of {len(files)}")
