0f925d34dc461c8bf4b08e8116457c4e05adf611
Jeff Shen <jshen@flatironinstitute.org>
2024-07-10 00:27:26 -0400

    remove unused files

--- scripts/gaia/test.sh
+++ scripts/gaia/test.sh
@@ -11,20 +11,24 @@ echo ========================
 # download files
 python3 download_parts.py --tiny --output_dir .
 
+echo =============================
+echo ADDING EXTRA INFO TO XP FILES
+echo =============================
+
+python3 add_extras.py --input_dir .
+
 echo ==================
-echo MERGING HDF5 FILES
+echo PARTITIONING FILES
 echo ==================
 
-# merge files
-python3 merge_parts.py --input_dir . --output_file merged.hdf5
+mkdir -p gaia
+python3 to_parquet.py --input_dir . --output_dir gaia --nside 16
 
-echo ===================
-echo HEALPIXIFYING FILES
-echo ===================
+echo ==================
+echo CONVERTING TO HDF5
+echo ==================
 
-# healpixify 
-mkdir gaia
-python3 healpixify.py --input_file merged.hdf5 --output_dir . --nside 8
+python3 to_hdf5.py --input_dir gaia
 
 echo ==================
 echo TESTING HF LOADING
