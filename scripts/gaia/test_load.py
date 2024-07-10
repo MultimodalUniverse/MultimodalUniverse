0600914bd972bfd6603d9dab8994bd295c21e87a
Jeff Shen <jshen@flatironinstitute.org>
2024-07-10 00:27:33 -0400

    bug fix

--- scripts/gaia/test_load.py
+++ scripts/gaia/test_load.py
@@ -1,28 +1,18 @@
 import datasets
 
 
-ds = datasets.load_dataset(
-    "./gaia.py",
-    trust_remote_code=True,
-    split="train",
-    streaming=True,
-).with_format("numpy")
+for k in ["dr3_source", "dr3_rvs", "dr3_xp"]:
+    ds = datasets.load_dataset(
+        "./gaia.py",
+        k,
+        trust_remote_code=True,
+        split="train",
+        streaming=True,
+    ).with_format("numpy")
 
-batch = next(iter(ds))
+    batch = next(iter(ds))
 
-print("testing spectral coefficients")
+    print(f"loaded {k} batch!")
 
-assert batch["spectral_coefficients"]["coeff"].size == 110
-assert batch["spectral_coefficients"]["coeff_error"].size == 110
 
-print("testing astrometry")
 
-for k in ["ra", "dec", "pmra", "pmdec", "parallax"]:
-    assert k in batch["astrometry"].keys()
-
-print("testing photometry")
-
-for k in ["bp_rp", "phot_g_mean_mag", "phot_bp_mean_mag", "phot_rp_mean_mag"]:
-    assert k in batch["photometry"].keys()
-
-print("Test passed!")
