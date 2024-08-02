04e74a4d52a7ee0a32595e50f7a2ec36699e0d7f
Jeff Shen <jshen@flatironinstitute.org>
2024-08-02 11:06:13 -0400

    add extra info to ap files too

--- scripts/gaia/add_extras.py
+++ scripts/gaia/add_extras.py
@@ -4,20 +4,21 @@ import os
 import argparse
 from tqdm.contrib.concurrent import process_map
 
-def transfer_extras(source_file_and_xp_file):
-    source_file, xp_file = source_file_and_xp_file
-    with h5py.File(xp_file, "r+") as f:
+def transfer_extras(source_file_and_dest_file):
+    source_file, dest_file = source_file_and_dest_file
+    with h5py.File(dest_file, "r+") as f:
         with h5py.File(source_file, "r") as g:
-            xp_id = f["source_id"][:]
+            dest_id = f["source_id"][:]
             source_id = g["source_id"][:]
-            _, xp_ix, source_ix = np.intersect1d(xp_id, source_id, assume_unique=True, return_indices=True)
-            assert len(source_ix) == len(xp_id), "Source IDs do not match"
-            assert np.all(np.diff(xp_ix) == 1), "XP IDs are not row matched"
+            _, dest_ix, source_ix = np.intersect1d(dest_id, source_id, assume_unique=True, return_indices=True)
+            assert len(source_ix) == len(dest_id), "Source IDs do not match"
+            assert np.all(np.diff(dest_ix) == 1), "Destination IDs are not row matched"
 
             for key in ["ra", "dec"]:
-                f.create_dataset(
-                    key, data=g[key][:][source_ix]
-                )
+                if key not in f.keys():
+                    f.create_dataset(
+                        key, data=g[key][:][source_ix]
+                    )
 
 
 def main(args):
@@ -31,14 +32,24 @@ def main(args):
         for f in os.listdir(args.input_dir)
         if f.startswith("XpContinuousMeanSpectrum") and f.endswith(".hdf5")
     ]
+    ap_files = [
+        os.path.join(args.input_dir, f)
+        for f in os.listdir(args.input_dir)
+        if f.startswith("AstrophysicalParameters") and f.endswith(".hdf5")
+    ]
 
     assert len(source_files) == len(
         xp_files
     ), "Number of source files and coefficient files do not match"
+    assert len(source_files) == len(
+        ap_files
+    ), "Number of source files and astrophysical parameter files do not match"
     source_files.sort()
     xp_files.sort()
+    ap_files.sort()
 
     process_map(transfer_extras, zip(source_files, xp_files), max_workers=args.num_workers, chunksize=1, desc="Adding extra info to XP files", total=len(source_files))
+    process_map(transfer_extras, zip(source_files, ap_files), max_workers=args.num_workers, chunksize=1, desc="Adding extra info to AP files", total=len(source_files))
 
 
 if __name__ == "__main__":
