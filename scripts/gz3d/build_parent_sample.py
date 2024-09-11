6ac4a3421d65e87827351e246664b7df1b8c842b
mb010 <micah.bowles@physics.ox.ac.uk>
2024-09-11 22:44:22 +0100

    MVP.

--- scripts/gz3d/build_parent_sample.py
+++ scripts/gz3d/build_parent_sample.py
@@ -26,30 +26,38 @@ GZ3D_URL = "https://data.sdss.org/sas/dr17/env/MANGA_MORPHOLOGY/galaxyzoo3d/v4_0
 _utf8_filter_type = h5py.string_dtype('utf-8', 5)
 _healpix_nside = 16
 
-def build_filelist(page_url: str, download_link: str = '^.fits.gz$', limit: int = None):
+def is_link_to_fits_file(link_text, endswith='.fits.gz'):
+    if link_text is None:
+        return False
+    else:
+        return link_text.endswith(endswith)
+
+def build_filelist(page_url: str, limit: int = None):
     """ Build a list of relevant files to transfer """
     response = requests.get(page_url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    hrefs = soup.find_all('a', href=True)
+    hrefs = soup.find_all('a', string=is_link_to_fits_file)
+    # Get href attribute from each element as list
     files = []
 
     n_files = limit or len(hrefs)
-    for ref in hrefs[0:n_files]:
-        if re.search(download_link, ref['href']):
-            files.append(ref['href'])
+    for ref in hrefs:
+        files.append(ref['href'])
+        if len(files) >= n_files:
+            break
     
     return files
 
-def download_all_files(links, base_url, test=False, dir="./data"):
+def download_all_files(links, base_url, test=False, dir="./data", force=False):
     for link in tqdm(links):
         try:
-            download_file(base_url, link, local_dir=dir)
+            download_file(base_url, link, download_dir=dir, force=force)
         except Exception as e:
-            print(f"Error downloading {urljoin(base_url+link)}: {e}")
+            print(f"Error downloading {urljoin(base_url, link)}: {e}")
         if test:
             break
 
-def download_file(url: str, file_name: str, download_dir: str ='./data', chunk_size: int = 8192):
+def download_file(url: str, file_name: str, download_dir: str ='./data', chunk_size: int = 8192, force: bool = False):
     """Download a file from a URL to a local directory in chunks (streaming).
 
     Args:
@@ -64,8 +72,9 @@ def download_file(url: str, file_name: str, download_dir: str ='./data', chunk_s
 
     full_url = urljoin(url, file_name)
 
-    local_dir = pathlib.Path(local_dir)
     local_filename = download_dir / pathlib.Path(file_name)
+    if local_filename.exists() and not force:
+        return str(local_filename)
     local_filename.parent.mkdir(parents=True, exist_ok=True)
 
     with requests.get(full_url, stream=True) as r:
@@ -74,24 +83,20 @@ def download_file(url: str, file_name: str, download_dir: str ='./data', chunk_s
             for chunk in r.iter_content(chunk_size=chunk_size):
                 if chunk: # Skip empty chunks
                     f.write(chunk)
-    # Extract fits from gz
-    with gzip.open(local_filename, 'rb') as f_in:
-        with open(local_filename.replace('.gz', ''), 'wb') as f_out:
-            f_out.write(f_in.read())
     
-    return str(local_filename.replace('.gz', ''))  # Return the file path as a string
+    return str(local_filename)  # Return the file path as a string
 
-def create_dataframe_from_files(folder: str, file_name: str = 'reconstructed_gz3d_catalog_new.csv', force: bool = False):
+def create_dataframe_from_files(folder: str, file_name: str = 'reconstructed_gz3d_catalog.csv', force: bool = False):
     file_path = pathlib.Path(folder) / file_name
     os.makedirs(folder, exist_ok=True)
     if not force:
         try:
             df = pd.read_csv(file_path)
             return df
-        except:
-            print(f"Creating new dataframe. File not found at {file_path}")
+        except Exception as e:
+            print(f"Creating new dataframe. File not found at {file_path}: {e}")
             pass
-    locs = list(glob.glob(f'{folder}*.fits.gz'))
+    locs = list(glob.glob(f"{folder}/*.fits.gz"))
     data = []
     for loc in tqdm(locs):
         manga_metadata_for_galaxy = fits.open(loc)[5].data
@@ -131,21 +136,20 @@ def generate_all_hdf5(df, nside=_healpix_nside, output_dir='output', local_data_
         os.makedirs(hpix_dir, exist_ok=True)
 
         for _, row in group.iterrows():
-            fits_file = os.path.join(f'{local_data_path_prefix}fits_gz', row['relative_gz3d_fits_loc'].split('/')[-1])
+            print(_, row)
+            fits_file = row['relative_gz3d_fits_loc']
             output_file = os.path.join(hpix_dir, f"{os.path.basename(fits_file).replace('.fits', '.h5').rstrip('.gz')}")
 
             # Read FITS file
             with fits.open(fits_file) as hdul:
                 with h5py.File(output_file, 'w') as hdf:
-                # Assuming the main data is in the primary HDU
-                    print(hdul.info())
-                    wcs = WCS(hdul[1].header) # TODO: Decide how to encode this
-                    hdf.attrs['wcs'] = wcs.to_header_string()
-                    hdf.create_dataset('image',       data=hdul[0].data)
-                    hdf.create_dataset('center_mask', data=hdul[1].data)
-                    hdf.create_dataset('star_mask',   data=hdul[2].data)
-                    hdf.create_dataset('spiral_mask', data=hdul[3].data)
-                    hdf.create_dataset('bar_mask',    data=hdul[4].data)                    
+                    wcs = WCS(hdul[1].header)
+                    hdf.attrs['wcs'] = wcs.to_header_string() # TODO: Decide how to encode this better.
+                    hdf.create_dataset('image',  data=hdul[0].data)
+                    hdf.create_dataset('center', data=hdul[1].data)
+                    hdf.create_dataset('star',   data=hdul[2].data)
+                    hdf.create_dataset('spiral', data=hdul[3].data)
+                    hdf.create_dataset('bar',    data=hdul[4].data)                    
 
     print(f"Conversion complete. Files saved in {output_dir}")
 
@@ -157,24 +161,24 @@ def main(args):
     else:
         os.makedirs(args.output_dir, exist_ok=True)
         files = build_filelist(GZ3D_URL, limit=limit)
-        download_all_files(files, GZ3D_URL, dir=args.output_dir)
+        download_all_files(files, GZ3D_URL, dir=args.output_dir, force=False)
         # Create a dataframe from the files
-        df = create_dataframe_from_files(f"{args.output_dir}/gz3d/")
+        df = create_dataframe_from_files(args.output_dir)
 
     # Process the files, i.e. make chosen cuts
-    df = data_selection(df)
+    if not args.tiny:
+        df = data_selection(df)
 
     # Generate HDF5 files
     generate_all_hdf5(df, output_dir=args.output_dir, local_data_path_prefix=args.local_data_path, limit=limit)
 
- 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extracts GZ3D data based on SDSS MaNGA files')
     parser.add_argument('--local_data_path', type=str, help='', default='')
     parser.add_argument('-o', '--output_dir', type=str, default='data', help='Path to the output directory')
     # parser.add_argument('-n', '--num_processes', type=int, default=8, help='The number of processes to use for parallel processing')
-    parser.add_argument('--tiny', action="store_true", help='Use a small subset of the data for testing')
+    parser.add_argument('--tiny', action="store_true", default=False, help='Use a small subset of the data for testing')
     args = parser.parse_args()
 
     main(args)
