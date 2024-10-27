be39de60eb72810670132879609b492ea29ae6bc
Brian Rogers <brianrogers0401@gmail.com>
2024-10-27 21:22:50 +0000

    Added changes to the Dataset generator for TGLC

--- scripts/tess/tess.py
+++ scripts/tess/tess.py
@@ -71,7 +71,7 @@ class TESS(datasets.GeneratorBasedBuilder):
             description="TESS-SPOC light curves (S0064)",
         )
     ]
-
+    
     DEFAULT_CONFIG_NAME = "all"
 
     @classmethod
