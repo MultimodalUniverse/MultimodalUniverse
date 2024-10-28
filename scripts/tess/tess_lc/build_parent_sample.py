a627e6a802cf216621c92261348471cf77cf9f7b
Brian Rogers <brianrogers0401@gmail.com>
2024-10-28 14:55:53 +0000

    Full pipeline with parallelized MAST downloads.

--- scripts/tess/tess_lc/build_parent_sample.py
+++ scripts/tess/tess_lc/build_parent_sample.py
@@ -7,16 +7,14 @@ import healpy as hp
 from multiprocessing import Pool
 from tqdm import tqdm
 import subprocess
+from datasets import load_dataset
 
 _healpix_nside = 16
 
 PIPELINES = ['TGLC']
 
 # To-Do:
-# - Scripts for downloading the .sh files and .csv from TGLC MAST
 # - Cross-matching across sectors with gaia_id 
-# - Write function to create .sh files
-
 
 def read_sh(fp: str):
     '''
@@ -95,7 +93,7 @@ def parse_curl_commands(sh_file):
     params = list(parse_line(line) for line in lines)
     return params 
 
-def create_sector_catalog(sector: int, tess_data_path: str, tiny: bool = True):
+def create_sector_catalog(sector: int, tess_data_path: str, save_catalog: bool = False, tiny: bool = True):
     '''
     Create a sector catalog from the .sh file. Sector catalogs contains: gaiadr3_id, cam, ccd, fp1, fp2, fp3, fp4 
     for each light curve in the sector.
@@ -104,23 +102,38 @@ def create_sector_catalog(sector: int, tess_data_path: str, tiny: bool = True):
     ----------
     sector: int, sector number
     tess_data_path: str, path to the directory containing the light curve fits files
-    tiny: bool, if True, only use a small sample for testing
+    tiny: bool, if True, only use a small sample of 100 objects for testing
+
     Returns
     ------- 
     catalog: astropy Table, sector catalog
     '''
 
     sector_str = f's{sector:04d}'
-    sh_fp = os.path.join(tess_data_path, f'{sector_str}/fits/hlsp_tglc_tess_ffi_{sector_str}_tess_v1_llc.sh')
+    sh_fp = os.path.join(tess_data_path, f'{sector_str}/{sector_str}_fits_download_script.sh')
     params = parse_curl_commands(sh_fp)
     column_names = ['gaiadr3_id', 'cam', 'ccd', 'fp1', 'fp2', 'fp3', 'fp4']
     catalog = Table(rows=params, names=column_names)
-    if tiny:
-        catalog = catalog[0:20]
 
+    if tiny:
+        catalog = catalog[0:100]
+
+    # Merge with target list to get RA-DEC
+    csv_fp = os.path.join(tess_data_path, f'{sector_str}/{sector_str}_target_list.csv')
+    target_list = Table.read(csv_fp, format='csv')
+    target_list.rename_column('#GAIADR3_ID', 'gaiadr3_id')
+
+    catalog = join(catalog, target_list, keys='gaiadr3_id', join_type='inner')
+
+    catalog['healpix'] = hp.ang2pix(_healpix_nside, catalog['RA'], catalog['DEC'], lonlat=True, nest=True)
+
+    if save_catalog:
+        output_fp = os.path.join(tess_data_path, f'{sector_str}', f'{sector_str}-catalog{"_tiny" if tiny else ""}.hdf5')
+        catalog.write(output_fp, format='hdf5', overwrite=True, path=output_fp)
+        print(f"Saved catalog to {output_fp}")
     return catalog
 
-def get_fits_lightcurve(curl_cmd, output_fp):
+def get_fits_lightcurve(catalog_row, sector, output_dir):
     '''
     Download the light curve file using the curl command and save it to the output file
 
@@ -134,12 +147,23 @@ def get_fits_lightcurve(curl_cmd, output_fp):
     success: bool, True if the download was successful, False otherwise
     '''
 
+    sector_str = f's{sector:04d}'
+    path = f'cam{catalog_row["cam"]}-ccd{catalog_row["ccd"]}/{catalog_row["fp1"]}/{catalog_row["fp2"]}/{catalog_row["fp3"]}/{catalog_row["fp4"]}/hlsp_tglc_tess_ffi_gaiaid-{catalog_row["gaiadr3_id"]}-{sector_str}-cam{catalog_row["cam"]}-ccd{catalog_row["ccd"]}_tess_v1_llc.fits'
+    url = f'https://archive.stsci.edu/hlsps/tglc/{sector_str}/' + path 
+
+    output_fp = os.path.join(output_dir, path)
+
+    # Create output directory if it doesn't exist
+    os.makedirs(os.path.dirname(output_fp), exist_ok=True)
+
+    cmd = f'wget {url} -O {output_fp}'
     try:
-        subprocess.run(curl_cmd, shell=True, check=True, text=True, capture_output=True)
+        subprocess.run(cmd, shell=True, check=True, text=True, capture_output=True)
         return True, f"Successfully downloaded: {output_fp}"
+    
     except subprocess.CalledProcessError as e:
-        return False, f"Error downloading using the following cmd: {curl_cmd}: {e.stderr}"
- 
+        return False, f"Error downloading using the following cmd: {cmd}: {e.stderr}"
+
 def processing_fn(
         row
 ):
@@ -232,47 +256,162 @@ def save_in_standard_format(args):
             hdf5_file.create_dataset(key, data=lightcurves[key])
     return 1
 
+def download_sh_script(sector: int, output_path: str, show_progress: bool = False):
+    '''
+    Download the sh script from the TGLC MAST site (https://archive.stsci.edu/hlsp/tglc)
 
-def main(output_dir = './test', tess_data_path = './tess_data', tiny = True, n_processes = 4):
-    
-    sector = 23
+    Parameters
+    ----------
+    sector: int, TESS sector number
+    output_path: str, path to the output directory
+    show_progress: bool, if True, show the progress of the download
+
+    Returns
+    -------
+    success: bool, True if the file was downloaded successfully, False otherwise
+
+    Raises
+    ------
+    Exception: if there is an error downloading the file
+    ''' 
+
     sector_str = f's{sector:04d}'
+    url = f'https://archive.stsci.edu/hlsps/tglc/download_scripts/hlsp_tglc_tess_ffi_{sector_str}_tess_v1_llc.sh'
 
-    # Download the sh file from the TGLC site
-    
-    catalog = create_sector_catalog(23, tess_data_path = "./tess_data", tiny = tiny)
-
-    lcs = Table.read(os.path.join(tess_data_path, "./s0023/s0023.csv")) # Do this for all sectors
-    lcs.rename_column('#GAIADR3_ID', 'gaiadr3_id')
-
-    lcs['healpix'] = hp.ang2pix(_healpix_nside, lcs['RA'], lcs['DEC'], lonlat=True, nest=True)
-   
-    # Join the catalogs using the gaia_id
-    catalog = join(catalog, lcs, keys='gaiadr3_id', join_type='inner')
-    #catalog.write(os.path.join(tess_data_path, f'{sector_str}', f'{sector_str}-catalog.hdf5'), format='hdf5', overwrite=True, path=os.path.join(tess_data_path, f'{sector_str}', f'{sector_str}-catalog.hdf5'))
-
-    catalog = catalog[0:20] # use small sample for testing 
-    output_fp = os.path.join(tess_data_path, f'{sector_str}', f'lcs') # output filepath of the fits light curves
-    
-    #parallel_args = [(curl_cmd, output_fp) for curl_cmd in read_sh(os.path.join(tess_data_path, f'{sector_str}', f'hlsp_tglc_tess_ffi_{sector_str}_tess_v1_llc.sh'))]
-    #with Pool(n_processes) as pool: # adjust the number of avaialble cores depending on CPU allocation
+    try:
+        os.makedirs(output_path, exist_ok=True)
+        curl_cmd = f'wget {"--progress=bar:force --show-progress" if show_progress else ""} {url} -O {os.path.join(output_path, f"{sector_str}_fits_download_script.sh")}'
         
-        #results = list(tqdm(pool.starmap(get_fits_lightcurve, parallel_args), total=len(parallel_args)))
-    
-    #successful = [r for r, _ in results if r]
-    #print(f"Successfully downloaded {len(successful)} out of {len(catalog)} files.")
-    #print(len(parallel_args))
+        if show_progress: # This could be cleaner
+            result = subprocess.run(curl_cmd, shell=True, check=True, text=True)
+        else:
+            result = subprocess.run(curl_cmd, shell=True, check=True, text=True, capture_output=True)
 
-    # Now lets convert the fits light curves to the standard format
-    output_dir = f'./tess_data/{sector_str}/lcs' # rename
+        if result.returncode == 0:
+            print(f"Successfully downloaded: {output_path}")
+            return True
+        else:
+            print(f"Error downloading file: {result.stderr}")
+            return False
+        
+    except Exception as e:
+        print(f"Error downloading .sh file from {url}: {e}")
+        return
+    
+def download_target_csv_file(sector: int, output_path: str, show_progress: bool = False):
+
+    '''
+    Download the target list csv file from the TGLC MAST site (https://archive.stsci.edu/hlsp/tglc)
+
+    Parameters
+    ----------
+    sector: int, TESS sector number
+    output_path: str, path to the output directory
+    show_progress: bool, if True, show the progress of the download
+
+    Returns
+    -------
+    success: bool, True if the file was downloaded successfully, False otherwise
+
+    '''
+    sector_str = f's{sector:04d}'
+    url = f'https://archive.stsci.edu/hlsps/tglc/target_lists/{sector_str}.csv'
+
+    try:
+        os.makedirs(output_path, exist_ok=True)
+        fp = os.path.join(output_path, f"{sector_str}_target_list.csv")
+                     
+        curl_cmd = f'wget {"--progress=bar:force --show-progress" if show_progress else ""} {url} -O {fp}'
+        
+        if show_progress:
+            result = subprocess.run(curl_cmd, shell=True, check=True, text=True)
+        else:
+            result = subprocess.run(curl_cmd, shell=True, check=True, text=True, capture_output=True)
+
+        if result.returncode == 0:
+            print(f"Successfully downloaded: {fp}")
+            return True
+        else:
+            print(f"Error downloading file: {result.stderr}")
+            return False
+        
+    except Exception as e:
+        print(f"Error downloading csv file from {url}: {e}")
+        return False
+    
+def download_sector_catalog_lightcurves(
+        sector: int, 
+        tess_data_path: str,
+        n_processes: int = 4,
+        tiny: bool = True
+    ):
+    '''
+    Download the light curves for a given sector and save them in the standard format
+
+    Parameters
+    ----------
+    sector: int, TESS sector number
+    tess_data_path: str, path to the output directory
+    n_processes: int, number of processes to use for the parallel download
+    tiny: bool, if True, only use a small sample of 100 objects for testing
+
+    Returns
+    -------
+    results: list, list of tuples containing the success status and the message for each light curve download
+    '''
+    
+
+    sector_str = f's{sector:04d}'
+    catalog_fp = os.path.join(tess_data_path, f'{sector_str}', f'{sector_str}-catalog{"_tiny" if tiny else ""}.hdf5')
+    
+    # check if sector catalog exists - if not, create it
+    if os.path.exists(catalog_fp):
+        print(f"Loading existing catalog from {catalog_fp}")
+        catalog = Table.read(catalog_fp, format='hdf5')
+
+    else:
+        print(f"Creating new catalog for sector {sector}")
+        catalog = create_sector_catalog(sector, tess_data_path, save_catalog = False, tiny = False)
+
+    # check if fits_lc path exists or make it
+
+    fits_lc_path = os.path.join(tess_data_path, f'{sector_str}/fits_lcs')
+    if not os.path.exists(fits_lc_path):
+        os.makedirs(fits_lc_path, exist_ok=True)
+    
+    # Check what the non-parallel version time is...
+
+    with Pool(n_processes) as pool:
+        results = list(tqdm(pool.starmap(get_fits_lightcurve, [(row, sector, fits_lc_path) for row in catalog]), total=len(catalog)))
+
+    if sum([result[0] for result in results]) != len(catalog):
+        print("There was an error in the parallel processing of the download of the fits files, some files may not have been downloaded.")
+
+    return results
+
+def convert_fits_to_standard_format(sector: int, catalog: Table, tess_data_path: str, n_processes: int = 4):
+    '''
+    Convert the fits light curves to the standard format and save them in a hdf5 file
+
+    Parameters
+    ----------
+    sector: int, TESS sector number
+    catalog: astropy.Table, sector catalog
+    tess_data_path: str, path to the output directory
+    n_processes: int, number of processes to use for the parallel processing
+
+    Returns
+    -------
+    results: list, list of booleans indicating the success of the conversion for each light curve
+    '''
+
+    sector_str = f's{sector:04d}'
+    output_dir = f'./tess_data/{sector_str}/MultimodalUniverse' # rename?
 
     catalog = catalog.group_by(['healpix'])
 
     map_args = []
-
     for group in catalog.groups: 
-        print(len(group), group['healpix'][0])
-        # Create a filename for the group
         group_filename = os.path.join(output_dir, '{}/healpix={}/001-of-001.hdf5'.format("TGLC", group['healpix'][0]))
         map_args.append((group, group_filename, tess_data_path))
 
@@ -280,10 +419,65 @@ def main(output_dir = './test', tess_data_path = './tess_data', tiny = True, n_p
         results = list(tqdm(pool.imap(save_in_standard_format, map_args), total=len(map_args)))
     
     if sum(results) != len(map_args):
-        print("There was an error in the parallel processing, some files may not have been processed correctly")
+        print("There was an error in the parallel processing of the fits files to standard format, some files may not have been processed correctly")
+    return results
 
-    print("All done!")
-    return 
+def main(output_dir = './tess_data/', tess_data_path = './tess_data', tiny = True, n_processes = 4):
+    '''
+    Full function to download the light curves for a given sector and save them in the standard format in hdf5 files
+
+    Parameters
+    ----------
+    output_dir: str, path to the output directory
+    tess_data_path: str, path to the tess data directory
+    tiny: bool, if True, only use a small sample of 100 objects for testing
+    n_processes: int, number of processes to use for the parallel processing
+
+    Returns
+    -------
+    None
+    '''
+
+    # Download the sh file from the TGLC site
+
+    download_sh_script(23, os.path.join(output_dir, 'sh_file'), show_progress = False) 
+
+    # Download the target list csv file from the TGLC site
+    download_target_csv_file(23, os.path.join(output_dir, 's0023/'), show_progress = False)
+
+    # Create the sector catalog
+    catalog = create_sector_catalog(23, tess_data_path = output_dir, save_catalog = True, tiny = True)  
+
+    # Download the fits light curves using the sector catalog
+    download_sector_catalog_lightcurves(23, output_dir, show_progress = True)
+
+    # count the number of files in the fits_lcs directory
+    # Count number of fits files
+
+    sector_str = f's{23:04d}'  # Using sector 23 as example
+    fits_dir = f'./tess_data/{sector_str}/fits_lcs'
+    
+    n_files = 0
+    for _, _, files in os.walk(fits_dir):
+        n_files += len([f for f in files if f.endswith('.fits')])
+    assert n_files == len(catalog), f"Expected {len(catalog)} .fits files in {fits_dir}, but found {n_files}"
+
+    # Process fits to standard format
+    convert_fits_to_standard_format(23, catalog, output_dir, n_processes = 4)
+
+    # How can I think of testing this robustly?
+    # Load to show the examples.
+    tess = load_dataset("./tess_data/s0023/MultimodalUniverse/tess.py", trust_remote_code=True)
+
+    dset = tess.with_format('numpy')['train']
+    print(dset)
+    example = next(iter(dset))
+    print(example)
+
+    print(example['lightcurve']['time'].shape)
+    print(example['lightcurve']['aper_flux'])
+    print(example['lightcurve']['aper_flux_err'])
+    # The formatting is wrong and the actual values are incorrect. This needs fixed.
 
 if __name__ == '__main__':
-    main(output_dir = './test', tess_data_path = './tess_data', tiny = False)
+    main(output_dir = './tess_data/', tess_data_path = './tess_data', tiny = False, n_processes = 4)
