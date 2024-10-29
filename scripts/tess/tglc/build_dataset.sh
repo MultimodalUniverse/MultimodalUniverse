f636b8c716367498e40eb0aa8834241050f87f90
Brian Rogers <brianrogers0401@gmail.com>
2024-10-29 10:14:10 +0000

    Fixed cmd line arguments for the .sh file

--- scripts/tess/tglc/build_dataset.sh
+++ scripts/tess/tglc/build_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Need a batcher for downloading the data from MAST
-if python build_parent_sample.py -s 23 tglc_data_path='./tglc_data' hdf5_output_path='./tglc_data/s0023/MultimodalUniverse' fits_output_path='./tglc_data/s0023/fits_lcs' --n_processes 4 --tiny; then
+if python build_parent_sample.py -s 23 --tglc_data_path './tglc_data' --hdf5_output_path './tglc_data/s0023/MultimodalUniverse' --fits_output_path './tglc_data/s0023/fits_lcs' --n_processes 4 --tiny; then
     echo "Build parent sample for TESS successful"
 else
     echo "Build parent sample  for TESS failed"
