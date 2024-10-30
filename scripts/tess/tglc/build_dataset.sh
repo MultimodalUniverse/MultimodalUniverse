092dbff41eb33e936ba0fa4b6ae02b26ff0ee0d3
Brian Rogers <brianrogers0401@gmail.com>
2024-10-30 17:03:33 +0000

    filepath changes to the .sh file

--- scripts/tess/tglc/build_dataset.sh
+++ scripts/tess/tglc/build_dataset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if python build_parent_sample.py -s 23 --tglc_data_path './tglc_data' --hdf5_output_path './tglc_data/s0023/MultimodalUniverse' --fits_output_path './tglc_data/s0023/fits_lcs' --n_processes 4 --tiny; then
+if python build_parent_sample.py -s 23 --tglc_data_path './tglc' --hdf5_output_path './tglc' --fits_output_path './tglc/fits' --n_processes 4 --tiny; then
     echo "Build for the TGLC parent sample was successful"
 else
     echo "Build parent sample for TGLC failed"
