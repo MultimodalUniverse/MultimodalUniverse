21c347894094ffac16417b06266a56a1c49b5800
Helen Qu <hqu10@rustyamd1.cm.cluster>
2024-10-30 21:03:24 -0400

    tess: changes for additional sectors

--- scripts/tess/README.md
+++ scripts/tess/README.md
@@ -5,31 +5,36 @@ a set of selection criteria for the TESS-SPOC light curves.
 
 ## Sample selection
 
-In the current version of the dataset, we retrieve all optical spectra from the TESS SPOC (Science Processing Operations Center) pipeline for sector 64. There are currently no specific cuts applied, but these could be specified in the mast_s3_transfer.py file.
+In the current version of the dataset, we retrieve all lightcurves from the TESS SPOC (Science Processing Operations Center) pipeline for sectors 56, 58, 60, 63, 64, 67, and 69. There are currently no specific cuts applied.
 
 ## Data preparation
 
-### Downloading data through MAST/Astroquery
+### Downloading data
 
-The first step to data preparation is to download all relevant data to a local machine through MAST. By default it will currently download one sector of data.
+The first step to data preparation is to download all relevant data to a local machine. The easiest way is to download the following scripts from the [official TESS site](https://archive.stsci.edu/hlsp/tess-spoc):
+- https://archive.stsci.edu/hlsps/tess-spoc/download_scripts/hlsp_tess-spoc_tess_phot_s0056_tess_v1_dl-lc.sh
+- https://archive.stsci.edu/hlsps/tess-spoc/download_scripts/hlsp_tess-spoc_tess_phot_s0058_tess_v1_dl-lc.sh
+- https://archive.stsci.edu/hlsps/tess-spoc/download_scripts/hlsp_tess-spoc_tess_phot_s0060_tess_v1_dl-lc.sh
+- https://archive.stsci.edu/hlsps/tess-spoc/download_scripts/hlsp_tess-spoc_tess_phot_s0063_tess_v1_dl-lc.sh
+- https://archive.stsci.edu/hlsps/tess-spoc/download_scripts/hlsp_tess-spoc_tess_phot_s0064_tess_v1_dl-lc.sh
+- https://archive.stsci.edu/hlsps/tess-spoc/download_scripts/hlsp_tess-spoc_tess_phot_s0067_tess_v1_dl-lc.sh
+- https://archive.stsci.edu/hlsps/tess-spoc/download_scripts/hlsp_tess-spoc_tess_phot_s0069_tess_v1_dl-lc.sh
 
-You can download the data through the following script
-```bash
-python mast_s3_transfer.py [--tiny] [path on your endpoint to download TESS data]
-```
-e.g. `python mast_s3_transfer.py --tiny ./tess_data/`
-This will submit a series of transfer requests.
-
-The total number of files downloaded should be around 160,000 per sector.
+These scripts will each download one sector of data into the directory you run the script from. These scripts can take quite a few hours so we'd recommend running them in a screen or tmux session. The total number of files downloaded should be around 160,000 per sector.
 
 ### Light curve extraction
 
-Once the TESS data has been downloaded, you can create the parent sample by running the following script:
+The above scripts will download each sector's data into a directory called `s00xx`, where `xx` is the sector number. All the lightcurves will be inside the `s00xx/target/0000` subdirectory. Once the data has been downloaded, you can create the parent sample by running the following script for each sector:
 ```bash
 python build_parent_sample.py [path to TESS data] [output directory] --num_processes [1] [--tiny];
 ```
-e.g. `python build_parent_sample.py ./tess_data/ ./tess_data_hdf5/ --num_processes 1 --tiny`
+e.g. `python build_parent_sample.py data/path/s00xx/target/0000 ./tess_s00xx_hdf5/ --num_processes 1 --tiny`
 
-### Documentation
+### Merge the sectors
 
-- TESS SPOC Data: https://archive.stsci.edu/hlsp/tess-spoc
+Finally, to put all the lightcurves together into a single directory, run the following script:
+```bash
+python merge_directories.py [path to one sector] [path to another sector] ...
+```
+e.g. `python merge_directories.py tess_s0056_hdf5 tess_s0058_hdf tess_s0060_hdf tess_s0063_hdf tess_s0064_hdf tess_s0067_hdf tess_s0069_hdf`
+
--- scripts/tess/build_parent_sample.py
+++ scripts/tess/build_parent_sample.py
@@ -1,14 +1,17 @@
 import os
 import argparse
 import numpy as np
+import pandas as pd
+import pdb
 from astropy.io import fits
 from astropy.table import Table, join
 from multiprocessing import Pool
 from tqdm import tqdm
 import h5py
 import healpy as hp
-from astropy.units import cds
 from quality import TESSQualityFlags
+from pathlib import Path
+import shutil
 
 _healpix_nside = 16
 
@@ -23,27 +26,26 @@ def processing_fn(args):
     with fits.open(filename, mode='readonly', memmap=True) as hdu:
         # The header of hdu[0] contains the following information:
         # Page 34, Section 5 - https://archive.stsci.edu/files/live/sites/mast/files/home/missions-and-data/active-missions/tess/_documents/EXP-TESS-ARC-ICD-TM-0014-Rev-F.pdf
+        try:
+            telescope = hdu[0].header.get('TELESCOP')
+            if telescope == 'TESS' and hdu[0].header.get('ORIGIN') == 'NASA/Ames':
+                targetid = hdu[0].header.get('TICID')
+                ra = hdu[0].header.get('RA_OBJ')
+                dec = hdu[0].header.get('DEC_OBJ')
 
-        telescope = hdu[0].header.get('TELESCOP')
-        if telescope == 'TESS' and hdu[0].header.get('ORIGIN') == 'NASA/Ames':
-            targetid = hdu[0].header.get('TICID')
-            ra = hdu[0].header.get('RA_OBJ')
-            dec = hdu[0].header.get('DEC_OBJ')
+                time = hdu['LIGHTCURVE'].data['TIME']
+                time_format = 'btjd'
+                # Units: BTJD (Barycenter corrected TESS Julian Date; BJD - 2457000, days)
 
-            time = hdu['LIGHTCURVE'].data['TIME']
-            time_format = 'btjd'
-            # Units: BTJD (Barycenter corrected TESS Julian Date; BJD - 2457000, days)
+                flux = hdu['LIGHTCURVE'].data['PDCSAP_FLUX']
+                flux_err = hdu['LIGHTCURVE'].data['PDCSAP_FLUX_ERR']
+                # Units: e-/s (electrons per second) -> can be read from the flux files, see the TESS data products documentation (TUNIT4)
 
-            flux = hdu['LIGHTCURVE'].data['PDCSAP_FLUX']
-            flux_err = hdu['LIGHTCURVE'].data['PDCSAP_FLUX_ERR']
-            # Units: e-/s (electrons per second) -> can be read from the flux files, see the TESS data products documentation (TUNIT4)
-
-            quality = np.asarray(hdu['LIGHTCURVE'].data['QUALITY'], dtype='int32')
-            quality_bitmask = TESSQualityFlags.DEFAULT_BITMASK
-        
-        # TODO: add support for other pipelines
-
-    # TODO: implement normalization option into relative flux (ppm)?
+                quality = np.asarray(hdu['LIGHTCURVE'].data['QUALITY'], dtype='int32')
+                quality_bitmask = TESSQualityFlags.DEFAULT_BITMASK
+        except Exception as e:
+            print(f"Error processing {filename}: {e}", flush=True)
+            return None
 
     exclude_bad_data = True
     if exclude_bad_data:
@@ -51,88 +53,120 @@ def processing_fn(args):
         time, flux, flux_err = time[indx], flux[indx], flux_err[indx]
 
     # Return the results
-    return {'object_id': object_id,
-            'time': time, 
-            'flux': flux, 
+    return {
+            'object_id': object_id,
+            'time': time,
+            'flux': flux,
             'flux_err': flux_err
             }
 
+# Open all files and extract the targetid, ra, dec
+def process_file(filename):
+    with fits.open(filename, mode='readonly', memmap=True) as hdu:
+        targetid = hdu[0].header.get('TICID')
+        ra = hdu[0].header.get('RA_OBJ')
+        dec = hdu[0].header.get('DEC_OBJ')
+    return targetid, ra, dec, filename
 
 def save_in_standard_format(args):
-    """ This function takes care of iterating through the different input files 
+    """ This function takes care of iterating through the different input files
     corresponding to this healpix index, and exporting the data in standard format.
     """
-    
-    catalog, output_filename, tess_data_path, tiny = args
+    catalog, output_filename = args
+    output_filename = Path(output_filename)
 
     # Create the output directory if it does not exist
-    if not os.path.exists(os.path.dirname(output_filename)):
-        os.makedirs(os.path.dirname(output_filename))
-
-    # Rename columns to match the standard format
-    catalog['object_id'] = catalog['target_name']
+    if output_filename.parent.exists():
+        print(f"output dir {output_filename.parent} already exists, deleting...")
+        shutil.rmtree(output_filename.parent)
+    output_filename.parent.mkdir(parents=True)
 
     # Process all files
     results = []
-    for args in catalog[['lc_path', 'object_id']]:
-        results.append(processing_fn(args))
+    for row in catalog[['filename', 'object_id']].itertuples():
+        results.append(processing_fn((row.filename, row.object_id)))
 
-    
     # Pad all light curves to the same length
-    max_length = max([len(d['time']) for d in results])
-    for i in range(len(results)):
-        results[i]['time'] = np.pad(results[i]['time'], (0,max_length - len(results[i]['time'])), mode='constant')
-        results[i]['flux'] = np.pad(results[i]['flux'], (0,max_length - len(results[i]['flux'])), mode='constant')
-        results[i]['flux_err'] = np.pad(results[i]['flux_err'], (0,max_length - len(results[i]['flux_err'])), mode='constant')
-    
-    # Aggregate all light curves into an astropy table
-    lightcurves = Table({k: [d[k] for d in results]
-                     for k in results[0].keys()})
+    results_cleaned = [d for d in results if d is not None]
+    print(f"number of light curves after cleaning: {len(results_cleaned)} / {len(results)}")
+    max_length = max([len(d['time']) for d in results_cleaned])
+    for i in range(len(results_cleaned)):
+        results_cleaned[i]['time'] = np.pad(results_cleaned[i]['time'], (0,max_length - len(results_cleaned[i]['time'])), mode='constant')
+        results_cleaned[i]['flux'] = np.pad(results_cleaned[i]['flux'], (0,max_length - len(results_cleaned[i]['flux'])), mode='constant')
+        results_cleaned[i]['flux_err'] = np.pad(results_cleaned[i]['flux_err'], (0,max_length - len(results_cleaned[i]['flux_err'])), mode='constant')
 
-    # Join on target id with the input catalog
-    catalog = join(catalog, lightcurves, keys='object_id', join_type='inner')
-    catalog.convert_unicode_to_bytestring()
-    
+    # Aggregate all light curves into an astropy table
+    lightcurves = Table({k: [d[k] for d in results_cleaned]
+                     for k in results_cleaned[0].keys()})
+    # Convert catalog to astropy table
+    catalog = catalog.drop(columns=['filename'])
+    if 'ra' in catalog.columns:
+        catalog = catalog.rename(columns={'ra': 'RA', 'dec': 'DEC'})
+    catalog_table = Table.from_pandas(catalog)
+
+    # Join catalog_table with lightcurves
+    catalog = join(catalog_table, lightcurves, keys='object_id', join_type='inner')
+
     # Making sure we didn't lose anyone
     assert len(catalog) == len(lightcurves), "There was an error in the join operation, probably some spectra files are missing"
 
     # Save all columns to disk in HDF5 format
     with h5py.File(output_filename, 'w') as hdf5_file:
         for key in catalog.colnames:
+            if catalog[key].dtype == 'object':
+                #TODO: time is a list of lists but nothing else is??
+                print(key)
+                # print(catalog[key].values[:5])
+                print(type(catalog[key][0]))
             hdf5_file.create_dataset(key, data=catalog[key])
     return 1
 
 def main(args):
-    # Load the catalog file and apply main cuts
-    catalog = Table.read(os.path.join(args.tess_data_path, "tess_lc_catalog_sector_64.csv"))
-    
-    # Add healpix index to the catalog
-    catalog['healpix'] = hp.ang2pix(_healpix_nside, catalog['RA'], catalog['DEC'], lonlat=True, nest=True)
+    output_dir = Path(args.output_dir)
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
 
-    #TODO: add support for multiple pipelines, currently only using SPOC
-    for pipeline in PIPELINES:
-        print("Processing pipeline:", pipeline)
+    # First write a catalog of all sources
+    if (output_dir / 'catalog.csv').exists():
+        catalog = pd.read_csv(output_dir / 'catalog.csv')
+        print(f"catalog with {len(catalog)} rows loaded from {output_dir / 'catalog.csv'}")
+    else:
+        print("catalog not found, creating new catalog")
+        files_generator = Path(args.tess_data_path).glob("**/*.fits")
 
-        cat_pipeline = catalog
-        #cat_pipeline = catalog[catalog['PIPELINE'] == pipeline]
-        
-        cat_pipeline = cat_pipeline.group_by(['healpix'])
-    
-        map_args = []
-        for group in cat_pipeline.groups:
-            # Create a filename for the group
-            group_filename = os.path.join(args.output_dir, '{}/healpix={}/001-of-001.hdf5'.format(pipeline.strip(), group['healpix'][0]))
-            
-            map_args.append((group, group_filename, args.tess_data_path, args.tiny))
+        with Pool(processes=args.num_processes) as pool:
+            results = list(tqdm(pool.imap(process_file, files_generator, chunksize=10_000),
+                                desc="creating catalog from files"))
 
-        # Run the parallel processing
-        with Pool(args.num_processes) as pool:
-            results = list(tqdm(pool.imap(save_in_standard_format, map_args), total=len(map_args)))
+        targetids, ras, decs, filenames = zip(*results)
 
-        if sum(results) != len(map_args):
-            print("There was an error in the parallel processing, some files may not have been processed correctly")
+        catalog = pd.DataFrame({
+            "object_id": targetids,
+            "RA": ras,
+            "DEC": decs,
+            "filename": filenames
+        })
+        catalog['healpix'] = hp.ang2pix(_healpix_nside, catalog['RA'], catalog['DEC'], lonlat=True, nest=True)
 
-        print("All done!")
+        # save to csv
+        catalog.to_csv(output_dir / 'catalog.csv', index=False)
+        print(f"Catalog saved to {str(output_dir / 'catalog.csv')}")
+
+    # Now process the lightcurves using the info in the catalog
+    map_args = []
+    # Group by healpix
+    df_grouped = catalog.groupby('healpix')
+    for i, (_, group) in enumerate(df_grouped):
+        # Create a filename for the group
+        group_filename = output_dir / 'spoc/healpix={}/001-of-001.hdf5'.format(group['healpix'].iloc[0])
+        map_args.append((group, group_filename))
+
+    # Run the parallel processing
+    with Pool(args.num_processes) as pool:
+        results = list(tqdm(pool.imap(save_in_standard_format, map_args), total=len(map_args)))
+
+    if sum(results) != len(map_args):
+        print("There was an error in the parallel processing, some files may not have been processed correctly")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extracts light curves from all TESS light curves downloaded from MAST')
