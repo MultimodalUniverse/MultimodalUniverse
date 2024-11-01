4189f3c1ad7b09ede4a1aa006be4fb00285eceac
Brian Rogers <brianrogers0401@gmail.com>
2024-11-01 16:57:22 +0000

    .sh file update

--- scripts/tess/pipelines/build_test.sh
+++ scripts/tess/pipelines/build_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-if python build_parent_sample.py --pipeline qlp -s 23 --data_path './tiny_qlp' --hdf5_output_path './tiny_qlp' --fits_output_path './tiny_qlp/fits' --n_processes 4 --tiny; then
-    echo "Build for the QLP parent sample was successful"
+if python build_parent_sample.py --pipeline spoc -s 23 --data_path './tiny_spoc' --hdf5_output_path './tiny_spoc' --fits_output_path './tiny_spoc/fits' --n_processes 6 --tiny; then
+    echo "Build for the SPOC parent sample was successful"
 else
-    echo "Build parent sample for QLP failed"
+    echo "Build parent sample for SPOC failed"
     exit 1
 fi
 
-if python -c "from datasets import load_dataset; from datasets.data_files import DataFilesPatternsDict; data_files = DataFilesPatternsDict.from_patterns({'train': ['./tiny_qlp/QLP/healpix=*/*.hdf5']}); dset = load_dataset('./qlp.py', trust_remote_code=True, split='train', streaming='true', data_files=data_files).with_format('numpy'); print('loaded dataset'); next(iter(dset));"; then
-    echo "Load dataset for QLP successful"
+if python -c "from datasets import load_dataset; from datasets.data_files import DataFilesPatternsDict; data_files = DataFilesPatternsDict.from_patterns({'train': ['./tiny_spoc/SPOC/healpix=*/*.hdf5']}); dset = load_dataset('./spoc.py', trust_remote_code=True, split='train', streaming='true', data_files=data_files).with_format('numpy'); print('loaded dataset'); next(iter(dset));"; then
+    echo "Load dataset for SPOC successful"
 else
-    echo "Load dataset for QLP failed"
+    echo "Load dataset for SPOC failed"
     exit 1
 fi
