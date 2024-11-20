770950e844d2b81548df63380412cfac73bcaa12
Jeff Shen <jshen@flatironinstitute.org>
2024-11-20 11:55:33 -0500

    parallelize healpixifying/partitioning

--- scripts/allwise/healpixify.py
+++ scripts/allwise/healpixify.py
@@ -1,17 +1,26 @@
+import os
+
 import healpy as hp
 import pyarrow.parquet as pq
+from tqdm.contrib.concurrent import process_map
 
 
 def ang2pix(ra, dec):
     return hp.ang2pix(nside=args.nside, theta=ra, phi=dec, lonlat=True, nest=True)
 
 
-def main(args):
-    table = pq.read_table(args.input_dir)
+def process_fragment(fragment):
+    table = fragment.to_table()
     hpix = ang2pix(table["ra"], table["dec"])
-    table = table.drop_columns(["healpix_k0", "healpix_k5"])
     table = table.append_column("healpix", [hpix])
     pq.write_to_dataset(table, args.output_dir, partition_cols=["healpix"])
+    return 1
+
+
+def main(args):
+    ds = pq.ParquetDataset(args.input_dir)
+    tables = process_map(process_fragment, ds.fragments, max_workers=os.cpu_count())
+    assert all(tables), "Some tables failed to process"
 
 
 if __name__ == "__main__":
