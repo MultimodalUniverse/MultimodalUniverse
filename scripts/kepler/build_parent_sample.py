4380f0b35119cfcbafdc6878dc29a61fb00bf085
IlayMalinyak <malinyakealy@gmail.com>
2024-12-10 16:08:51 +0200

    data download partial

--- scripts/kepler/build_parent_sample.py
+++ scripts/kepler/build_parent_sample.py
@@ -6,64 +6,78 @@ from astropy.table import Table, join
 from multiprocessing import Pool
 from tqdm import tqdm
 import h5py
-import healpy as hp
-from astropy.units import cds
-from quality import TESSQualityFlags
+# import healpy as hp
 
 _healpix_nside = 16
 
-# Breakdown of the different TESS pipelines, each one will be stored as a subdataset
-PIPELINES = ['spoc  ']
+# Breakdown of the different Kepler pipelines
+PIPELINES = ['sap', 'pdc']
+
 
 def processing_fn(args):
-    """ Parallel processing function reading all requested light curves from one sector.
+    """ Parallel processing function reading all requested light curves.
     """
     filename, object_id = args
 
     with fits.open(filename, mode='readonly', memmap=True) as hdu:
-        # The header of hdu[0] contains the following information:
-        # Page 34, Section 5 - https://archive.stsci.edu/files/live/sites/mast/files/home/missions-and-data/active-missions/tess/_documents/EXP-TESS-ARC-ICD-TM-0014-Rev-F.pdf
-
+        # Kepler header parsing
         telescope = hdu[0].header.get('TELESCOP')
-        if telescope == 'TESS' and hdu[0].header.get('ORIGIN') == 'NASA/Ames':
-            targetid = hdu[0].header.get('TICID')
+        if telescope == 'Kepler' and hdu[0].header.get('ORIGIN') == 'NASA/Ames':
+            # Kepler-specific header information extraction
+            targetid = hdu[0].header.get('KEPLERID')
             ra = hdu[0].header.get('RA_OBJ')
             dec = hdu[0].header.get('DEC_OBJ')
+            obsmode = hdu[0].header.get('OBSMODE')
 
-            time = hdu['LIGHTCURVE'].data['TIME']
-            time_format = 'btjd'
-            # Units: BTJD (Barycenter corrected TESS Julian Date; BJD - 2457000, days)
+            # Time handling for Kepler (Kepler Julian Date)
+            time = hdu[1].data['TIME']
+            time_format = hdu[1].data['TUNIT1']
+            # Units: Kepler Julian Date
+            # Two flux options: SAP (Simple Aperture Photometry) and PDCSAP (Pre-Search Data Conditioning)
+            sap_flux = hdu[1].data['SAP_FLUX']
+            sap_flux_err = hdu[1].data['SAP_FLUX_ERR']
+            pdcsap_flux = hdu[1].data['PDCSAP_FLUX']
+            pdcsap_flux_err = hdu[1].data['PDCSAP_FLUX_ERR']
 
-            flux = hdu['LIGHTCURVE'].data['PDCSAP_FLUX']
-            flux_err = hdu['LIGHTCURVE'].data['PDCSAP_FLUX_ERR']
-            # Units: e-/s (electrons per second) -> can be read from the flux files, see the TESS data products documentation (TUNIT4)
+            # Quality flags for Kepler
+            quality = np.asarray(hdu[1].data['SAP_QUALITY'], dtype='int32')
 
-            quality = np.asarray(hdu['LIGHTCURVE'].data['QUALITY'], dtype='int32')
-            quality_bitmask = TESSQualityFlags.DEFAULT_BITMASK
-        
-        # TODO: add support for other pipelines
-
-    # TODO: implement normalization option into relative flux (ppm)?
-
+    # Basic quality filtering
     exclude_bad_data = True
     if exclude_bad_data:
-        indx = TESSQualityFlags.filter(quality, flags=quality_bitmask)
-        time, flux, flux_err = time[indx], flux[indx], flux_err[indx]
+        # Basic quality filtering (you may need to adjust these flags)
+        good_data_mask = (quality == 0) & \
+                         np.isfinite(time) & \
+                         np.isfinite(sap_flux) & \
+                         np.isfinite(sap_flux_err) & \
+                         np.isfinite(pdcsap_flux) & \
+                         np.isfinite(pdcsap_flux_err)
+
+        time = time[good_data_mask]
+        sap_flux = sap_flux[good_data_mask]
+        sap_flux_err = sap_flux_err[good_data_mask]
+        pdcsap_flux = pdcsap_flux[good_data_mask]
+        pdcsap_flux_err = pdcsap_flux_err[good_data_mask]
 
     # Return the results
-    return {'object_id': object_id,
-            'time': time, 
-            'flux': flux, 
-            'flux_err': flux_err
-            }
+    return {
+        'object_id': object_id,
+        'time': time,
+        'sap_flux': sap_flux,
+        'sap_flux_err': sap_flux_err,
+        'pdcsap_flux': pdcsap_flux,
+        'pdcsap_flux_err': pdcsap_flux_err,
+        'ra': ra,
+        'dec': dec,
+        'cadence': obsmode
+    }
 
 
 def save_in_standard_format(args):
-    """ This function takes care of iterating through the different input files 
-    corresponding to this healpix index, and exporting the data in standard format.
+    """ Process Kepler light curves and save in standard format.
     """
-    
-    catalog, output_filename, tess_data_path, tiny = args
+
+    catalog, output_filename, kepler_data_path, tiny = args
 
     # Create the output directory if it does not exist
     if not os.path.exists(os.path.dirname(output_filename)):
@@ -77,24 +91,34 @@ def save_in_standard_format(args):
     for args in catalog[['lc_path', 'object_id']]:
         results.append(processing_fn(args))
 
-    
     # Pad all light curves to the same length
     max_length = max([len(d['time']) for d in results])
     for i in range(len(results)):
-        results[i]['time'] = np.pad(results[i]['time'], (0,max_length - len(results[i]['time'])), mode='constant')
-        results[i]['flux'] = np.pad(results[i]['flux'], (0,max_length - len(results[i]['flux'])), mode='constant')
-        results[i]['flux_err'] = np.pad(results[i]['flux_err'], (0,max_length - len(results[i]['flux_err'])), mode='constant')
-    
+        results[i]['time'] = np.pad(results[i]['time'], (0, max_length - len(results[i]['time'])), mode='constant',
+                                    constant_values=np.nan)
+        results[i]['sap_flux'] = np.pad(results[i]['sap_flux'], (0, max_length - len(results[i]['sap_flux'])), mode='constant',
+                                    constant_values=np.nan)
+        results[i]['sap_flux_err'] = np.pad(results[i]['sap_flux_err'], (0, max_length - len(results[i]['sap_flux_err'])),
+                                        mode='constant', constant_values=np.nan)
+        results[i]['pdcsap_flux'] = np.pad(results[i]['pdcsap_flux'], (0, max_length - len(results[i]['pdcsap_flux'])),
+                                        mode='constant',
+                                        constant_values=np.nan)
+        results[i]['pdcsap_flux_err'] = np.pad(results[i]['pdcsap_flux_err'],
+                                            (0, max_length - len(results[i]['pdcsap_flux_err'])),
+                                            mode='constant', constant_values=np.nan)
+
+
     # Aggregate all light curves into an astropy table
     lightcurves = Table({k: [d[k] for d in results]
-                     for k in results[0].keys()})
+                         for k in results[0].keys()})
 
     # Join on target id with the input catalog
     catalog = join(catalog, lightcurves, keys='object_id', join_type='inner')
     catalog.convert_unicode_to_bytestring()
-    
+
     # Making sure we didn't lose anyone
-    assert len(catalog) == len(lightcurves), "There was an error in the join operation, probably some spectra files are missing"
+    assert len(catalog) == len(
+        lightcurves), "There was an error in the join operation, probably some light curve files are missing"
 
     # Save all columns to disk in HDF5 format
     with h5py.File(output_filename, 'w') as hdf5_file:
@@ -102,28 +126,29 @@ def save_in_standard_format(args):
             hdf5_file.create_dataset(key, data=catalog[key])
     return 1
 
-def main(args):
-    # Load the catalog file and apply main cuts
-    catalog = Table.read(os.path.join(args.tess_data_path, "tess_lc_catalog_sector_64.csv"))
-    
-    # Add healpix index to the catalog
-    catalog['healpix'] = hp.ang2pix(_healpix_nside, catalog['RA'], catalog['DEC'], lonlat=True, nest=True)
 
-    #TODO: add support for multiple pipelines, currently only using SPOC
+def main(args):
+    # Load the catalog file
+    catalog = Table.read(os.path.join(args.kepler_data_path, f"kepler_lc_catalog.csv"))
+
+    # Add healpix index to the catalog
+    # catalog['healpix'] = hp.ang2pix(_healpix_nside, catalog['RA'], catalog['DEC'], lonlat=True, nest=True)
+
+    # Process for each pipeline
     for pipeline in PIPELINES:
         print("Processing pipeline:", pipeline)
 
+        # Filter catalog by pipeline if needed
         cat_pipeline = catalog
-        #cat_pipeline = catalog[catalog['PIPELINE'] == pipeline]
-        
         cat_pipeline = cat_pipeline.group_by(['healpix'])
-    
+
         map_args = []
         for group in cat_pipeline.groups:
             # Create a filename for the group
-            group_filename = os.path.join(args.output_dir, '{}/healpix={}/001-of-001.hdf5'.format(pipeline.strip(), group['healpix'][0]))
-            
-            map_args.append((group, group_filename, args.tess_data_path, args.tiny))
+            group_filename = os.path.join(args.output_dir,
+                                          '{}/healpix={}/001-of-001.hdf5'.format(pipeline, group['healpix'][0]))
+
+            map_args.append((group, group_filename, args.kepler_data_path, args.tiny))
 
         # Run the parallel processing
         with Pool(args.num_processes) as pool:
@@ -134,11 +159,14 @@ def main(args):
 
         print("All done!")
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Extracts light curves from all TESS light curves downloaded from MAST')
-    parser.add_argument('tess_data_path', type=str, help='Path to the local copy of the TESS data')
+    parser = argparse.ArgumentParser(description='Extracts light curves from Kepler data downloaded from MAST')
+    parser.add_argument('kepler_data_path', type=str, help='Path to the local copy of the Kepler data')
     parser.add_argument('output_dir', type=str, help='Path to the output directory')
-    parser.add_argument('-nproc', '--num_processes', type=int, default=10, help='The number of processes to use for parallel processing')
+    parser.add_argument('-q', '--quarter', type=int, required=True, help='Kepler Quarter to process')
+    parser.add_argument('-nproc', '--num_processes', type=int, default=10,
+                        help='The number of processes to use for parallel processing')
     parser.add_argument('--tiny', action='store_true', help='Use a tiny subset of the data for testing')
     args = parser.parse_args()
 
--- scripts/kepler/mast_s3_transfer.py
+++ scripts/kepler/mast_s3_transfer.py
@@ -8,91 +8,107 @@ from tqdm import tqdm
 import argparse
 from pathlib import Path
 
-PIPELINE = 'TESS-SPOC'
-CADENCE = [120.0,120.0]
+PIPELINE = 'KEPLER'
+LONG_CADENCE = "Lightcurve Long Cadence"
+SHORT_CADENCE = "Lightcurve Short Cadence"
 
-# TESS Sector numbers for Primary Mission, Extended Mission 1, and Extended Mission 2
-PM = list(range(1, 26 + 1))
-EM1 = list(range(27, 55 + 1))
-EM2 = list(range(56, 96 + 1))
+# Kepler Quarters range from 0 to 17
+QUARTERS = list(range(0, 18))
+
 
 def batcher(seq, batch_size):
     return (seq[pos:pos + batch_size] for pos in range(0, len(seq), batch_size))
 
-def main(args):
-    SECTOR = args.sector
 
-    obs_table = Observations.query_criteria(provenance_name=PIPELINE,
-                                            #t_exptime=CADENCE,
-                                            sequence_number=SECTOR,
-                                            dataproduct_type='timeseries',
-                                            dataRights='PUBLIC')
+def main(args):
+    QUARTER = args.quarter
+    CADENCE = LONG_CADENCE if args.cadence == 'long' else SHORT_CADENCE
+    CADENCE_SUFFIX = '_llc.fits' if args.cadence == 'long' else '_slc.fits'
+
+    # If a specific quarter is provided, query for that quarter
+    if QUARTER is not None:
+        print(f"Querying for Quarter: {QUARTER}, Cadence: {'Long' if args.cadence == 'long' else 'Short'}")
+        obs_table = Observations.query_criteria(provenance_name=PIPELINE,
+                                                dataproduct_type='timeseries',
+                                                dataRights='PUBLIC')
+    else:
+        # If no quarter specified, query all Kepler data
+        obs_table = Observations.query_criteria(provenance_name=PIPELINE,
+                                                dataproduct_type='timeseries',
+                                                dataRights='PUBLIC')
+
+    obs_table = Observations.filter_products(
+        obs_table,
+        productType="SCIENCE",
+        description=CADENCE
+    )
+    print(f"Observation table: {len(obs_table)}")
+    exit()
 
     # Only download a small sample of the data for testing purposes
     if args.tiny:
         products = Observations.get_product_list(obs_table[:10])
     else:
-        # Retrieve the product list in batches because the query is often too large for MAST to handle in one go, resulting in time out errors.
+        # Retrieve the product list in batches to avoid timeout errors
         batch_size = 5000
         table_len = len(obs_table)
-        print('Retrieving data product download links for {} targets in batches of {}'.format(table_len, batch_size))
+        print(f'Retrieving data product download links for {table_len} targets in batches of {batch_size}')
         try:
-            product_list = [Observations.get_product_list(batch) for batch in tqdm(batcher(obs_table, batch_size), total = table_len // batch_size)]
+            product_list = [Observations.get_product_list(batch) for batch in
+                            tqdm(batcher(obs_table, batch_size), total=table_len // batch_size)]
             products = vstack(product_list)
         except requests.exceptions.Timeout as e:
             print(e)
             print('Request timed out. Trying again...')
             time.sleep(3)
-            product_list = [Observations.get_product_list(batch) for batch in tqdm(batcher(obs_table, batch_size), total = table_len // batch_size)]
+            product_list = [Observations.get_product_list(batch) for batch in
+                            tqdm(batcher(obs_table, batch_size), total=table_len // batch_size)]
             products = vstack(product_list)
 
-    manifest = Observations.download_products(products, extension="_lc.fits", download_dir=args.output_path, flat=True)
+    # Download light curve files
+    manifest = Observations.download_products(products, extension=CADENCE_SUFFIX, download_dir=args.output_path,
+                                              flat=True)
 
     # Build a catalog based on the downloaded data
     catalog = manifest.to_pandas()
-    catalog['productFilename'] = catalog['Local Path'].str.split('/', expand=True).iloc[:,-1:]
+    catalog['productFilename'] = catalog['Local Path'].str.split('/', expand=True).iloc[:, -1:]
     catalog = catalog.merge(products.to_pandas(), on='productFilename')
-    catalog = catalog[['Local Path','Status','productFilename', 'dataURI']]
+    catalog = catalog[['Local Path', 'Status', 'productFilename', 'dataURI']]
     catalog.rename(columns={'dataURI': 'dataURL', 'Local Path': 'lc_path'}, inplace=True)
     catalog = catalog.merge(obs_table.to_pandas(), on='dataURL')
 
-    # Only keep the targets for which the light curves were downloaded succesfully
-    catalog = catalog[catalog['Status']=='COMPLETE'].reset_index(drop=True)
-    catalog.rename(columns={'s_ra': 'RA','s_dec': 'DEC'}, inplace=True)
-    catalog['sector'] = SECTOR
+    # Only keep the targets for which the light curves were downloaded successfully
+    catalog = catalog[catalog['Status'] == 'COMPLETE'].reset_index(drop=True)
+    catalog.rename(columns={'s_ra': 'RA', 's_dec': 'DEC'}, inplace=True)
+    catalog['quarter'] = QUARTER if QUARTER is not None else 'All'
     catalog['pipeline'] = PIPELINE
+    catalog['cadence'] = args.cadence
 
-    # Optional - replace relative with absolute path
-    catalog['lc_path'] = catalog['lc_path'].apply(lambda x: Path(x).resolve()) 
-    
-    catalog = catalog[['lc_path','target_name','RA','DEC','sector','pipeline']]
+    # Replace relative with absolute path
+    catalog['lc_path'] = catalog['lc_path'].apply(lambda x: Path(x).resolve())
 
-    # TODO: query TESS Input Catalog (TIC) for all other available parameters:
-    # catalog_data = Catalogs.query_criteria(catalog="TIC",Tmag=[10,10.1], objType="STAR")
+    catalog = catalog[['lc_path', 'target_name', 'RA', 'DEC', 'quarter', 'pipeline', 'cadence']]
 
     # Save the catalog
-    catalog.to_csv(args.output_path + '/tess_lc_catalog_sector_' + str(SECTOR) + '.csv', index=None)
+    output_filename = f'kepler_lc_catalog_quarter_{QUARTER}_{args.cadence}_cadence.csv' if QUARTER is not None else f'kepler_lc_catalog_all_quarters_{args.cadence}_cadence.csv'
+    catalog.to_csv(Path(args.output_path) / output_filename, index=None)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Downloads TESS data to a user-provided endpoint.")
-
-    # TODO: implement pipeline argument. Note: QLP Pipeline requires different download script.
-    # parser.add_argument("TESS Pipeline", type=str, help="TESS Data Pipeline to download.")
-    # TODO: add download option per mission cycle (Primary Mission, Extended Mission 1, Extended Mission 2)
+    parser = argparse.ArgumentParser(description="Downloads Kepler light curve data to a user-provided endpoint.")
 
     parser.add_argument('output_path', type=str, help='Path to save the data')
     parser.add_argument('--tiny', action='store_true', help='Use a tiny subset of the data for testing')
-
-    #TODO: adjust urllib3 number of retries in case of a download failure (which is common in mast/astroquery)
-    parser.add_argument('-s', '--sector', type=int, default=64, help="TESS Sector to download.")
+    parser.add_argument('-q', '--quarter', type=int, default=None,
+                        help="Kepler Quarter to download (0-17). If not specified, downloads all quarters.")
+    parser.add_argument('-c', '--cadence', type=str, choices=['long', 'short'], default='long',
+                        help="Choose light curve cadence. 'long' (default) or 'short'.")
 
     args = parser.parse_args()
 
-    if not Path(args.output_path).exists():
-        Path(args.output_path).mkdir(parents=True)
-        print('Created directory:', args.output_path)
-    else:
-        print('Output directory already exists:', args.output_path)
-    
+    # Create output directory if it doesn't exist
+    output_path = Path(args.output_path)
+    output_path.mkdir(parents=True, exist_ok=True)
+    print(f'Output directory: {output_path}')
+
     main(args)
