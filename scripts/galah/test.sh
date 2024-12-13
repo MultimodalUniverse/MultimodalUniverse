b83c87da11596e2d5c2ef57bb5311fb354fc758f
Jeff Shen <shenjeff@princeton.edu>
2024-12-13 13:05:58 -0800

    use python instead of python3

--- scripts/galah/test.sh
+++ scripts/galah/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 download.py --tiny
+python download.py --tiny
 
 cd data/spectra
 
@@ -8,6 +8,6 @@ cd data/spectra
 
 cd ../..
 
-python3 build_parent_sample.py --data_dir data/spectra/spectra --allstar_file data/GALAH_DR3_main_allstar_v2.fits --resolution_map_dir data --vac_file data/GALAH_DR3_VAC_ages_v2.fits  --missing_id_file data/GALAH_DR3_list_missing_reduced_spectra_v2.csv --output_dir ./dr3 --nside 16 --num_workers 4 --tiny
+python build_parent_sample.py --data_dir data/spectra/spectra --allstar_file data/GALAH_DR3_main_allstar_v2.fits --resolution_map_dir data --vac_file data/GALAH_DR3_VAC_ages_v2.fits  --missing_id_file data/GALAH_DR3_list_missing_reduced_spectra_v2.csv --output_dir ./dr3 --nside 16 --num_workers 4 --tiny
 
-python3 test_load.py
+python test_load.py
