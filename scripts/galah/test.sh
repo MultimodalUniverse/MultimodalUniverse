118944c109922de52b2efe763dc4c2582c4cfe57
Jeff Shen <jshen2014@hotmail.com>
2024-12-13 14:47:03 -0800

    Update scripts/galah/test.sh
    Co-authored-by: Micah Bowles <micah.bowles@physics.ox.ac.uk>

--- scripts/galah/test.sh
+++ scripts/galah/test.sh
@@ -18,6 +18,11 @@ fi
 
 cd ../..
 
-python build_parent_sample.py --data_dir data/spectra/spectra --allstar_file data/GALAH_DR3_main_allstar_v2.fits --resolution_map_dir data --vac_file data/GALAH_DR3_VAC_ages_v2.fits  --missing_id_file data/GALAH_DR3_list_missing_reduced_spectra_v2.csv --output_dir ./dr3 --nside 16 --num_workers 4 --tiny
+if python build_parent_sample.py --data_dir data/spectra/spectra --allstar_file data/GALAH_DR3_main_allstar_v2.fits --resolution_map_dir data --vac_file data/GALAH_DR3_VAC_ages_v2.fits  --missing_id_file data/GALAH_DR3_list_missing_reduced_spectra_v2.csv --output_dir ./dr3 --nside 16 --num_workers 4 --tiny; then
+    echo "Build parent sample for GALAH successful"
+else
+    echo "Build parent sample for GALAH failed"
+    exit 1
+fi
 
 python test_load.py
