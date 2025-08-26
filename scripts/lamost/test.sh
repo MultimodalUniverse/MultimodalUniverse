e067de783dabe66b69103dee6737dc4a20ffbf25
Jeff Shen <shenjeff@princeton.edu>
2025-08-26 15:27:59 -0400

    update test

--- scripts/lamost/test.sh
+++ scripts/lamost/test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-python3 download_data.py --catalog_name LRS_cv --release dr10_v2.0 -o ./lrs_cv_spectra -i 50 -n 8 -d 1.0 --timeout 30 --retries 3
-python3 build_parent_sample.py dr10_v2.0_LRS_cv.fits ./lrs_cv_spectra/ . --tiny
+CATALOG=LRS_wd # also try LRS_cv
+
+python3 download_data.py --catalog_name ${CATALOG} --release dr10_v2.0 -o ./${CATALOG,,}_spectra -i 50 -n 8 -d 1.0 --timeout 30 --retries 3
+python3 build_parent_sample.py dr10_v2.0_${CATALOG}.fits ./${CATALOG,,}_spectra/ . --tiny
 
 python3 -c "
 try:
     from datasets import load_dataset
 
-    ds = load_dataset('lamost.py', name='dr10_v20_lrs_cv', trust_remote_code=True, split='train')
+    ds = load_dataset('lamost.py', name='dr10_v20_${CATALOG,,}', trust_remote_code=True, split='train')
 
     batch = ds[0]
 
